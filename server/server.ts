412f8ac2bcbea89a80fd8d90c9bc0daf6110b4f8
kevin <kevinwang2040@gmail.com>
2021-02-05 18:45:46 -0500

    Modify server.ts to allow running server

--- server/server.ts
+++ server/server.ts
@@ -1,40 +1,39 @@
-import mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const { ApolloServer, gql } = require('apollo-server');
 
 //-----------------------------------------------------------------------------
 // MONGODB + MONGOOSE
 //-----------------------------------------------------------------------------
 
-const uri = "";
+// const uri = "";
 
-/*mongoose
-    .connect(uri)
-    .then(() => {})
-    .catch(err => console.log(err));*/
+// /*mongoose
+//     .connect(uri)
+//     .then(() => {})
+//     .catch(err => console.log(err));*/
 
 
-//-----------------------------------------------------------------------------
-// APOLLO SETUP
-//-----------------------------------------------------------------------------
+// //-----------------------------------------------------------------------------
+// // APOLLO SETUP
+// //-----------------------------------------------------------------------------
 
 const typeDefs = gql`
     type Book {
         title: String
         author: String
     }
+    type Query {
+        title: String
+    }
 `;
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
-};
+
 
 //-----------------------------------------------------------------------------
 // SERVER LAUNCH
 //-----------------------------------------------------------------------------
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen({ port: process.env.PORT }).then(({ url }) => {
+const server = new ApolloServer({ typeDefs });
+const port = process.env.PORT || 4000;
+server.listen({ port }).then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
 });
