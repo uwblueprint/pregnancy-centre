022b1882fc070a957a61f0fd89f30d76a99ba357
jlight99 <ellenhuang523@gmail.com>
2021-02-28 17:07:50 -0500

    refactor common modal to include styling; refactor confirmation modal to use common modal

--- client/src/components/organisms/CommonModal.tsx
+++ client/src/components/organisms/CommonModal.tsx
@@ -5,30 +5,29 @@ import { useDispatch } from "react-redux";
 import { Button, Modal } from 'react-bootstrap';
 
 function CommonModal(props: any) {
-    const [show, setShow] = useState(true);
+  const [show, setShow] = useState(true);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return ( 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                <Modal.Title>{props.modalTitle}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    {props.body}
-                </Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                </Button>
-                </Modal.Footer>
-            </Modal>
-    );
-  }
-  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Modal show={show} onHide={handleClose} centered={true} className="modal">
+      <Modal.Header closeButton>
+      </Modal.Header>
+      <Modal.Body>
+        <div>
+          <img src="TPC_leaf.png"></img>
+        </div>
+        <div className="title">
+          {props.title}
+        </div>
+        {props.body}
+      </Modal.Body>
+      <Modal.Footer>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 export default CommonModal;
 
--- client/src/pages/ConfirmationModal.tsx
+++ client/src/pages/ConfirmationModal.tsx
@@ -1,48 +1,32 @@
 import './Modal.scss';
-import React, { ChangeEvent, FormEvent, useState } from 'react';
-import { Modal } from 'react-bootstrap';
+import React, { useState } from 'react';
+import CommonModal from '../components/organisms/CommonModal';
 
 function ConfirmationModal(props: any) {
   // TODO(ellen): email should be passed in from props
   const [email, setEmail] = useState("anna@pregnancycentre.ca");
-  const [show, setShow] = useState(true);
-
-  const handleClose = () => setShow(false);
 
   return (
-    <Modal show={show} onHide={handleClose} centered={true} className="modal">
-      <Modal.Header closeButton>
-      </Modal.Header>
-      <Modal.Body>
-        <span>
-        <div>
-          <img src="TPC_leaf.png"></img>
+    <CommonModal title={"Confirm Your Email"} body={
+      <span>
+        <div className="text">
+          We’ve just sent an email to the following email address.
         </div>
-        <div className="title">
-          Confirm Your Email
+        <div className="text link">
+          {email}
         </div>
-          <div className="text">
-            We’ve just sent an email to the following email address.
-          </div>
-          <div className="text link">
-            {email}
-          </div>
-          <div className="text">
-            You’ll have X time to open the confirmation link in the email to confirm that you own the email address.
-          </div>
-          <div className="text">
-            You’ll then be able to login as an employee.
-          </div>
-          <button className="button" onClick={handleClose}>
-            I understand
-          </button>
-        </span>
-      </Modal.Body>
-      <Modal.Footer>
-      </Modal.Footer>
-    </Modal>
+        <div className="text">
+          You’ll have X time to open the confirmation link in the email to confirm that you own the email address.
+        </div>
+        <div className="text">
+          You’ll then be able to login as an employee.
+        </div>
+        <button className="button">
+          I understand
+        </button>
+      </span>
+    }></CommonModal>
   );
 }
 
 export default ConfirmationModal;
-
