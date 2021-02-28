c70f61a0a34121df8ed31882f0b8509ba7a45488
jlight99 <ellenhuang523@gmail.com>
2021-02-28 16:40:46 -0500

    styled confirmation modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,23 @@ import './App.scss';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import React from "react";
 
+import ConfirmationModal from "./pages/ConfirmationModal";
 import SampleContainer from "./components/examples/SampleContainer";
 import SignUp from './pages/SignUp';
 import SignUpModal from "./pages/SignUpModal";
 
 function App(): JSX.Element {
   return (
-    <Router>
+    <span>
+      <Router>
       <Switch>
         <Route path='/test' component={SampleContainer}></Route>
         <Route path='/signup' strict component={SignUpModal}></Route>
         <Route path='/signuptest' strict component={SignUp}></Route>
+        <Route path='/confirmation' component={ConfirmationModal}></Route>
       </Switch>
     </Router>
+    </span>
   );
 }
 
--- client/src/pages/ConfirmationModal.tsx
+++ client/src/pages/ConfirmationModal.tsx
@@ -1,47 +1,46 @@
+import './Modal.scss';
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { Modal } from 'react-bootstrap';
 
 function ConfirmationModal(props: any) {
-
+  // TODO(ellen): email should be passed in from props
   const [email, setEmail] = useState("anna@pregnancycentre.ca");
   const [show, setShow] = useState(true);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const handleClick = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
-
 
   return (
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={show} onHide={handleClose} centered={true} className="modal">
       <Modal.Header closeButton>
-        <Modal.Title>Confirm Your Email</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <span>
-
-          <div>
+        <div>
+          <img src="TPC_leaf.png"></img>
+        </div>
+        <div className="title">
+          Confirm Your Email
+        </div>
+          <div className="text">
             We’ve just sent an email to the following email address.
-              </div>
-          <div>
+          </div>
+          <div className="text link">
             {email}
           </div>
-          <div>
+          <div className="text">
             You’ll have X time to open the confirmation link in the email to confirm that you own the email address.
+          </div>
+          <div className="text">
             You’ll then be able to login as an employee.
-              </div>
-          <button role="link">
+          </div>
+          <button className="button" onClick={handleClose}>
             I understand
-              </button>
+          </button>
         </span>
-
       </Modal.Body>
+      <Modal.Footer>
+      </Modal.Footer>
     </Modal>
-
-
-
   );
 }
 
