c9ccdf9251f04dd50bb54698e90d4f65ff49a416
chamod-gamage <chamodgamage26@gmail.com>
2021-02-28 16:20:47 -0500

    Auth messages out of function, expanded regex

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,39 +1,40 @@
 import firebase from "./firebase";
 
+interface AuthErrorMessageInterface {
+  "auth/invalid-email": string;
+  "auth/email-already-in-use": string;
+  "invalid-domain": string;
+  "invalid-password": string;
+}
+
+const AuthErrorMessage: AuthErrorMessageInterface = {
+  //firebase
+  "auth/invalid-email": "Invalid email.",
+  "auth/email-already-in-use": "That email has already been registered.",
+  //pre-firebase
+  "invalid-domain": "Invalid email domain.",
+  "invalid-password": "Please enter a valid password.",
+};
+
 export const createNewAccount = async (
   email: string,
   password: string
 ): Promise<{ email: string; password: string }> => {
-  //12 characters min., Contains: Upper, Lower, Number, Non-Alphanumeric
-  const passwordRequirements = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^?&*])(?=.{12,})/;
+  //12 characters min., Contains: Upper, Lower, Number, Non-Alphanumeric (see group below)
+  const passwordRequirements = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[*.! @#$%^&(){}[\]:;<>,.?/~_+-=|\\])(?=.{12,})/;
   let errors = {
     email: "",
     password: "",
   };
 
   //needed to make ts happy
-  interface errorMessageInterface {
-    "auth/invalid-email": string;
-    "auth/email-already-in-use": string;
-    "invalid-domain": string;
-    "invalid-password": string;
-  }
-
-  const errorMessageMap: errorMessageInterface = {
-    //firebase
-    "auth/invalid-email": "Invalid email.",
-    "auth/email-already-in-use": "That email has already been registered.",
-    //pre-firebase
-    "invalid-domain": "Invalid email domain.",
-    "invalid-password": "Please enter a valid password.",
-  };
 
   if (!passwordRequirements.test(password)) {
-    errors.password = errorMessageMap["invalid-password"];
+    errors.password = AuthErrorMessage["invalid-password"];
   }
-  if (!email.endsWith("@pregnancycentre.ca")) {
-    errors.email = errorMessageMap["invalid-domain"];
-  }
+  // if (!email.endsWith("@pregnancycentre.ca")) {
+  //   errors.email = AuthErrorMessage["invalid-domain"];
+  // }
 
   if (!errors.email.length && !errors.password.length) {
     errors = await firebase
@@ -45,8 +46,8 @@ export const createNewAccount = async (
         return { email: "", password: "" };
       })
       .catch((error) => {
-        const code: keyof errorMessageInterface = error.code;
-        return { email: errorMessageMap[code], password: "" };
+        const code: keyof AuthErrorMessageInterface = error.code;
+        return { email: AuthErrorMessage[code], password: "" };
       });
   }
 
