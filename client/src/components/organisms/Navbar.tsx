3254a9859ec6a38f0964e3c231847cd036e82f13
Megan Niu <megan.niu@gmail.com>
2021-02-28 13:29:37 -0500

    Fix linting errors

--- client/src/components/organisms/Navbar.tsx
+++ client/src/components/organisms/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 
 import BsNavbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
