520318d82178bd42f04cf746d63c4c6f9386e241
Megan Niu <megan.niu@gmail.com>
2021-03-04 18:12:19 -0500

    Read redux store in RequestGroupList

--- client/src/components/organisms/RequestGroupList.tsx
+++ client/src/components/organisms/RequestGroupList.tsx
@@ -1,31 +1,44 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
+import { connect } from "react-redux";
 
 import PageNavigation from '../atoms/PageNavigation'
 import RequestGroup from '../../data/types/requestGroup'
 import RequestGroupScrollWindow from '../molecules/RequestGroupScrollWindow'
+import { RootState } from '../../data/reducers'
 
-interface Props {
-  requestGroups: Array<RequestGroup>,
+interface StateProps {
+  requestGroups: Array<RequestGroup>
+}
+
+type Props = StateProps & {
   selectedRequestGroup: string | undefined,
-  pages: number,
-  currentPage: number, // Indexing starting at 1.
-  onPageChange: (newPage: number) => void,
   onRequestGroupChange: (requestGroupdId: string) => void,
 }
 
 const RequestGroupList: FunctionComponent<Props> = (props: Props) => {
+  const numGroupsPerPage = 10
+  const pages = Math.ceil(props.requestGroups.length / numGroupsPerPage)
+  const [currentPage, setCurrentPage] = useState(1) // Indexing starting at 1.
+
   return <div className="request-group-list">
     <RequestGroupScrollWindow
-      requestGroups={props.requestGroups}
+      requestGroups={props.requestGroups.slice((pages - 1) * numGroupsPerPage, Math.min(pages * numGroupsPerPage, props.requestGroups.length - 1))}
       selectedRequestGroup={props.selectedRequestGroup}
       onRequestGroupChange={props.onRequestGroupChange} />
     <div className="request-group-list-page-navigation">
       <PageNavigation
-        pages={props.pages}
-        currentPage={props.currentPage}
-        onPageChange={props.onPageChange} />
+        pages={pages}
+        currentPage={currentPage}
+        onPageChange={(newPage: number) => { setCurrentPage(newPage) }} />
     </div>
   </div>
 };
 
-export default RequestGroupList;
+const mapStateToProps = (store: RootState): StateProps => {
+  return {
+    requestGroups: store.requestGroups.data,
+  };
+};
+
+export default connect<StateProps, Record<string, unknown>, Record<string, unknown>, RootState>(mapStateToProps)(RequestGroupList);
+
