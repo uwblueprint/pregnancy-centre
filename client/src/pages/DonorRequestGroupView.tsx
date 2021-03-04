8bdbd0e30c82387fe6145a623dd8175d999c0f21
Megan Niu <megan.niu@gmail.com>
2021-03-04 18:11:34 -0500

    Add DonorRequestGroupView

---
+++ client/src/pages/DonorRequestGroupView.tsx
@@ -0,0 +1,71 @@
+import { bindActionCreators, Dispatch } from "redux"
+import { gql, useQuery } from "@apollo/client";
+import React, { FunctionComponent, useState } from "react";
+import { connect } from "react-redux";
+
+import { loadRequestGroups } from '../data/actions'
+import RequestGroup from '../data/types/requestGroup'
+import RequestGroupList from '../components/organisms/RequestGroupList'
+import { RootState } from '../data/reducers'
+
+// import Col from 'react-bootstrap/Col'
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+
+interface StateProps {
+  requestGroups: Array<RequestGroup>
+}
+
+interface DispatchProps {
+  loadRequestGroups: typeof loadRequestGroups,
+}
+
+
+type Props = StateProps & DispatchProps;
+
+const DonorRequestView: FunctionComponent<Props> = (props: React.PropsWithChildren<Props>) => {
+  const [selectedRequestGroup, setSelectedRequestGroup] = useState<string | undefined>(props.requestGroups.length <= 0 ? undefined : props.requestGroups[0]._id)
+
+  const query = gql`
+  {
+    requestGroups {
+      _id
+      name
+      dateUpdated
+      deleted
+      description
+      requirements
+      image
+      numOpen
+    }
+  }`
+
+  const { loading, error, data } = useQuery(query, {
+    onCompleted: (data: { requestGroups: Array<RequestGroup> }) => {
+      props.loadRequestGroups(data.requestGroups);
+    },
+  });
+
+
+  return <RequestGroupList
+    selectedRequestGroup={selectedRequestGroup}
+    onRequestGroupChange={(requestGroupdId: string) => { setSelectedRequestGroup(requestGroupdId) }}
+  />
+};
+
+const mapStateToProps = (store: RootState): StateProps => {
+  return {
+    requestGroups: store.requestGroups.data,
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
+  return bindActionCreators(
+    {
+      loadRequestGroups,
+    },
+    dispatch
+  );
+};
+
+export default connect<StateProps, DispatchProps, Record<string, unknown>, RootState>(mapStateToProps, mapDispatchToProps)(DonorRequestView);
