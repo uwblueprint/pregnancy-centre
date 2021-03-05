c0315024aac4559c184cdae1cbc6cf9b274ab7da
Megan Niu <megan.niu@gmail.com>
2021-03-04 19:55:03 -0500

    Set donor homepage to default route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,9 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import React from "react";
 
 import ConfirmationModal from "./pages/ConfirmationModal";
-import DonorRequestGroupBrowser from './components/organisms/DonorRequestGroupBrowser'
+import DonorHomepage from './pages/DonorHomepage'
 import EmailConfirmedModal from './pages/EmailConfirmedModal';
+import RequestGroupDonorView from './components/organisms/RequestGroupDonorView';
 import SampleContainer from "./components/examples/SampleContainer";
 import SignUpModal from "./pages/SignUpModal";
 
@@ -18,15 +19,15 @@ function App(): JSX.Element {
         <Route path='/confirmation' component={() => <ConfirmationModal email="anna@pregnancycentre.ca"/>}></Route>
         <Route path='/donation-guidelines'><SampleContainer /></Route>
         <Route path='/login'><SampleContainer /></Route>
-<<<<<<< HEAD
         {/* TODO(jlight99): delete /confirmation endpoint after logic for triggering the confirmation modal upon signup has been added */}
         <Route path='/confirmation' component={() => <ConfirmationModal email="anna@pregnancycentre.ca"/>}></Route>
         <Route path='/signup' strict component={SignUpModal}></Route>
         {/* TODO(jlight99): delete /email-confirmed endpoint after logic for triggering the email confirmed modal has been added */}
         <Route path='/email-confirmed' strict component={EmailConfirmedModal}></Route>
-=======
->>>>>>> Use DonorRequestGroupBrowser in homepage
-        <Route path='/'><DonorRequestGroupBrowser /></Route>
+        <Route path='/test'>
+          <RequestGroupDonorView requestGroupId="603d9b41eb57fc06447b8a23"/>
+        </Route>
+        <Route path='/'><DonorHomepage /></Route>
       </Switch>
     </Router>
     </span>
