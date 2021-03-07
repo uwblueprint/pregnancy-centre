9ae482f5ceca9b4d1ed9e48ff060887a4a9ba5b2
jlight99 <ellenhuang523@gmail.com>
2021-03-07 15:58:36 -0500

    add common modal files

---
+++ client/src/components/organisms/Modal.tsx
@@ -0,0 +1,28 @@
+import '../../pages/Modal.scss';
+import React, { useState } from 'react';
+import { useApolloClient } from "@apollo/client";
+import { useDispatch } from "react-redux";
+
+import { Button, Modal } from 'react-bootstrap';
+
+function CommonModal(props: any) {
+  return (
+    <Modal show={props.show} onHide={props.handleClose} centered={true} className="modal">
+      <Modal.Header closeButton>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="pc-icon">
+          <img src="TPC_leaf.png"></img>
+        </div>
+        <div className="title">
+          {props.title}
+        </div>
+        {props.body}
+      </Modal.Body>
+      <Modal.Footer>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default CommonModal;
