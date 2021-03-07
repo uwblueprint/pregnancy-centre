dd7e00124918804eca665688d0f4ad41251e4f87
Megan Niu <megan.niu@gmail.com>
2021-03-07 16:49:27 -0500

    Use DonorRequestGroupBrowser in homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,11 +18,14 @@ function App(): JSX.Element {
         <Route path='/confirmation' component={() => <ConfirmationModal email="anna@pregnancycentre.ca"/>}></Route>
         <Route path='/donation-guidelines'><SampleContainer /></Route>
         <Route path='/login'><SampleContainer /></Route>
+<<<<<<< HEAD
         {/* TODO(jlight99): delete /confirmation endpoint after logic for triggering the confirmation modal upon signup has been added */}
         <Route path='/confirmation' component={() => <ConfirmationModal email="anna@pregnancycentre.ca"/>}></Route>
         <Route path='/signup' strict component={SignUpModal}></Route>
         {/* TODO(jlight99): delete /email-confirmed endpoint after logic for triggering the email confirmed modal has been added */}
         <Route path='/email-confirmed' strict component={EmailConfirmedModal}></Route>
+=======
+>>>>>>> Use DonorRequestGroupBrowser in homepage
         <Route path='/'><DonorRequestGroupBrowser /></Route>
       </Switch>
     </Router>
