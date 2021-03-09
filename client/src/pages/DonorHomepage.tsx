eb70f2a6c7a30d2b05f952b8bd6b51a14c3c3656
Megan Niu <megan.niu@gmail.com>
2021-03-09 12:01:57 -0500

    Style DonorHomepage

--- client/src/pages/DonorHomepage.tsx
+++ client/src/pages/DonorHomepage.tsx
@@ -1,17 +1,16 @@
 import React, { FunctionComponent } from "react";
-import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
 import DonorPage from '../components/layouts/DonorPage'
-import DonorRequestGroupView from '../components/organisms/DonorRequestGroupView'
+import DonorRequestGroupBrowser from '../components/organisms/DonorRequestGroupBrowser'
 import HomepageBanner from '../components/organisms/HomepageBanner'
 
 const DonorHomepage: FunctionComponent = () => {
   return <Container className="donor-homepage" fluid>
     <DonorPage>
       <Row><HomepageBanner /></Row>
-      <Row className="donor-homepage-request-groups-view"><DonorRequestGroupView /></Row>
+      <Row className="donor-homepage-request-groups-browser"><DonorRequestGroupBrowser /></Row>
     </DonorPage>
   </Container>;
 };
