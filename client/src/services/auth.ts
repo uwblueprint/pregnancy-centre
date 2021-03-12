937fa4a25d0f2f0efaeb90776733c1d19cf04654
Chamod Gamage <chamodgamage26@gmail.com>
2021-03-11 20:35:00 -0500

    Update auth.ts

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -57,7 +57,7 @@ export const createNewAccount = async (
 export const handleVerifyEmail = async (
   actionCode: string
 ): Promise<string> => {
-  let error = await firebase
+  const error = await firebase
     .auth()
     .applyActionCode(actionCode)
     .then((resp: any) => {
