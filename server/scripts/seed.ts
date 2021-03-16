cd30827ee9aaf1d1285f5422a0b9a988d4b41010
jlight99 <ellenhuang523@gmail.com>
2021-03-16 00:41:59 -0400

    add in new fields, refactor; todo: test

--- server/scripts/seed.ts
+++ server/scripts/seed.ts
@@ -16,6 +16,85 @@ import { RequestType } from '../models/requestTypeModel'
 
 dotenv.config()
 
+const createClient = (clientIDs, clientIdx) => {
+  const client = new Client({
+    _id: mongoose.Types.ObjectId(),
+    clientId: faker.random.alphaNumeric(8),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName()
+  })
+  clientIDs.push(client._id) // store IDs to allocate among groups + types
+  const promise = client.save().catch((err) => {
+    if (err) {
+      console.error('\x1b[31m', 'Attempted to seed client # ' + clientIdx + ' but failed:')
+      console.log('\x1b[0m')
+      console.error(err)
+      exit()
+    }
+  })
+  return promise
+}
+
+const createRequest = (requestID, clientIDs, requestTypeID, requestIdx, groupIdx, typeIdx) => {
+  const request = new Request({
+    _id: requestID,
+    requestId: faker.random.alphaNumeric(6),
+    client: faker.random.arrayElement(clientIDs),
+    requestType: requestTypeID
+  })
+  const promise = request.save().catch((err) => {
+    if (err) {
+      console.error('\x1b[31m', 'Attempted to seed request # ' + requestIdx + ' for group ' + groupIdx + ', type ' + typeIdx + ' but failed:')
+      console.log('\x1b[0m')
+      console.error(err)
+      exit()
+    }
+  })
+  return promise
+}
+
+const createRequestType = (typeID, requestIDsForType, requestGroupID, typeIdx, groupIdx) => {
+  const type = new RequestType({
+    _id: typeID,
+    name: faker.commerce.product(),
+    requests: {
+      fulfilled: [],
+      deleted: [],
+      open: requestIDsForType
+    },
+    requestGroup: requestGroupID
+  })
+  const promise = type.save().catch((err) => {
+    if (err) {
+      console.error('\x1b[31m', 'Attempted to seed type #' + typeIdx + ' for group ' + groupIdx + ' but failed:')
+      console.log('\x1b[0m')
+      console.error(err)
+      exit()
+    }
+  })
+  return promise
+}
+
+const createRequestGroup = (groupID, typeIDs, groupIdx) => {
+  const group = new RequestGroup({
+    _id: groupID,
+    name: faker.commerce.department(),
+    description: faker.lorem.sentence(),
+    requirements: faker.lorem.sentence(),
+    image: 'https://picsum.photos/200',
+    requestTypes: typeIDs
+  })
+  const promise = group.save().catch((err) => {
+    if (err) {
+      console.error('\x1b[31m', 'Attempted to seed group #' + groupIdx + ' but failed:')
+      console.log('\x1b[0m')
+      console.error(err)
+      exit()
+    }
+  })
+  return promise
+}
+
 // connect to DB, and on success, seed documents
 connectDB(() => {
   console.log('\x1b[34m', 'Beginning to seed')
@@ -63,90 +142,24 @@ connectDB(() => {
   const promises = []
 
   const clientIDs = []
-  for (let i = 0; i < numClients; i++) {
-    const client = new Client({
-      _id: mongoose.Types.ObjectId(),
-      clientId: faker.random.alphaNumeric(8),
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName()
-    })
-    clientIDs.push(client._id) // store IDs to allocate among groups + types
-    const promise = client.save().catch((err) => {
-      if (err) {
-        console.error('\x1b[31m', 'Attempted to seed client # ' + i + ' but failed:')
-        console.log('\x1b[0m')
-        console.error(err)
-        exit()
-      }
-    })
-    promises.push(promise) // for tracking completion
+  for (let clientIdx = 0; clientIdx < numClients; clientIdx++) {
+    const clientPromise = createClient(clientIDs, clientIdx)
+    promises.push(clientPromise) // for tracking completion
   }
 
-  const requestIDs = []
   for (let groupIdx = 0; groupIdx < numGroups; groupIdx++) {
+    const groupID = mongoose.Types.ObjectId()
+    const typeIDsForGroup = []
     for (let typeIdx = 0; typeIdx < numTypesPerGroup; typeIdx++) {
+      typeIDsForGroup.push(mongoose.Types.ObjectId())
+      const requestIDsForType = []
       for (let requestIdx = 0; requestIdx < numRequestsPerType; requestIdx++) {
-        const request = new Request({
-          _id: mongoose.Types.ObjectId(),
-          requestId: faker.random.alphaNumeric(6),
-          client: faker.random.arrayElement(clientIDs)
-        })
-        requestIDs.push(request._id) // store IDs to allocate among groups + types
-        const promise = request.save().catch((err) => {
-          if (err) {
-            console.error('\x1b[31m', 'Attempted to seed request # ' + requestIdx + ' for group ' + groupIdx + ', type ' + typeIdx + ' but failed:')
-            console.log('\x1b[0m')
-            console.error(err)
-            exit()
-          }
-        })
-        promises.push(promise) // for tracking completion
+        requestIDsForType.push(mongoose.Types.ObjectId())
+        promises.push(createRequest(requestIDsForType[requestIdx], clientIDs, typeIDsForGroup[typeIdx], requestIdx, groupIdx, typeIdx))
       }
+      promises.push(createRequestType(typeIDsForGroup[typeIdx], requestIDsForType, groupID, typeIdx, groupIdx))
     }
-  }
-
-  const typeIDs = []
-  for (let groupIdx = 0; groupIdx < numGroups; groupIdx++) {
-    for (let typeIdx = 0; typeIdx < numTypesPerGroup; typeIdx++) {
-      const type = new RequestType({
-        _id: mongoose.Types.ObjectId(),
-        name: faker.commerce.product(),
-        requests: {
-          fulfilled: [],
-          deleted: [],
-          open: requestIDs
-        },
-      })
-      typeIDs.push(type._id) // store IDs to allocate among groups + types
-      const promise = type.save().catch((err) => {
-        if (err) {
-          console.error('\x1b[31m', 'Attempted to seed type #' + typeIdx + ' for group ' + groupIdx + ' but failed:')
-          console.log('\x1b[0m')
-          console.error(err)
-          exit()
-        }
-      })
-      promises.push(promise) // for tracking completion
-    }
-  }
-
-  for (let groupIdx = 0; groupIdx < numGroups; groupIdx++) {
-    const group = new RequestGroup({
-      name: faker.commerce.department(),
-      description: faker.lorem.sentence(),
-      requirements: faker.lorem.sentence(),
-      image: 'https://picsum.photos/200',
-      requestTypes: typeIDs
-    })
-    const promise = group.save().catch((err) => {
-      if (err) {
-        console.error('\x1b[31m', 'Attempted to seed group #' + groupIdx + ' but failed:')
-        console.log('\x1b[0m')
-        console.error(err)
-        exit()
-      }
-    })
-    promises.push(promise) // for tracking completion
+    promises.push(createRequestGroup(groupID, typeIDsForGroup, groupIdx))
   }
 
   Promise.all(promises).then(() => {
