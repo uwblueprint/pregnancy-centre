cb6ae425c1218565e2e418d9fe0fdaa0e354ee4c
kevin <kevinwang2040@gmail.com>
2021-03-16 17:22:37 -0400

    Revert requestDataSource

--- server/datasources/requestDataSource.ts
+++ server/datasources/requestDataSource.ts
@@ -1,45 +1,9 @@
 import CachedMongooseDataSource from './cachedMongooseDataSource'
 import { RequestCache } from '../database/cache'
 import { RequestDocument } from '../models/requestModel'
-import { ServerResponseInterface } from '../graphql/serverResponse'
-
-import { Types } from 'mongoose'
 
 export default class RequestDataSource extends CachedMongooseDataSource<RequestDocument> {
   constructor() {
     super(RequestCache)
-  }
-  
-  async createRequest(request: RequestDocument): Promise<ServerResponseInterface> {
-    return await super.create(request)
-      .then(res => {
-        return res
-      })
-      .catch(error => {
-        console.log(error)
-        return error
-      });
-  }
-  
-  async updateRequest(request: RequestDocument): Promise<ServerResponseInterface> {
-    return await super.update(request)
-      .then(res => {
-        return res
-      })
-      .catch(error => {
-        console.log(error)
-        return error
-      });
-  }
-
-  async softDeleteRequest(id: Types.ObjectId): Promise<ServerResponseInterface> {
-    return await super.softDelete(id)
-      .then(res => {
-        return res
-      })
-      .catch(error => {
-        console.log(error)
-        return error
-      });
   }
 }
