dbd3699fe127bbafe47db76aec7f19848fc1738e
kevin <kevinwang2040@gmail.com>
2021-03-16 22:21:42 -0400

    Removed unused imports

--- server/datasources/requestTypeDataSource.ts
+++ server/datasources/requestTypeDataSource.ts
@@ -1,9 +1,6 @@
 import CachedMongooseDataSource from './cachedMongooseDataSource'
 import { RequestTypeCache } from '../database/cache'
 import { RequestTypeDocument } from '../models/requestTypeModel'
-import { ServerResponseInterface } from '../graphql/serverResponse'
-
-import { Types } from 'mongoose'
 
 export default class RequestTypeDataSource extends CachedMongooseDataSource<RequestTypeDocument> {
   constructor() {
