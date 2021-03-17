e092428de24e13e714bbd46b2611345aa4f18080
Bonnie Chin <bonniechin.mail@gmail.com>
2021-03-16 23:18:17 -0400

    code styling

--- client/src/pages/PasswordResetEmailSentModal.tsx
+++ client/src/pages/PasswordResetEmailSentModal.tsx
@@ -12,8 +12,9 @@ const EmailConfirmedModal: FunctionComponent = () => {
       <span>
         <div className="text">
             Check your email and click on the password reset link to reset the password to your account.
-        <div className="text">
-            The link will expire after X days, after which you’ll have to request another reset link.</div>
+            <div className="text">
+                The link will expire after X days, after which you’ll have to request another reset link.
+            </div>
         </div>
         <button className="button" onClick={handleClose}>
           I understand
