5f18dd0885d9bcde5b202b04cd2d7334625de769
chamod-gamage <chamodgamage26@gmail.com>
2021-03-20 18:45:32 -0400

    SignInModal, add errors to signup modal, fix up modal styling

---
+++ client/src/pages/SignInModal.tsx
@@ -0,0 +1,113 @@
+import "./Modal.scss";
+import React, { FunctionComponent } from "react";
+import CommonModal from "../components/organisms/Modal";
+// import { createNewAccount } from "../services/auth";
+import { EyeFilled, EyeInvisibleFilled } from "@ant-design/icons";
+import { FormControl, InputGroup } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+import { useState } from "react";
+
+const SignInModal: FunctionComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleClose = () => setShow(false);
+  const [errors, setErrors] = useState({ email: "", password: "" });
+  const [show, setShow] = useState(true);
+  const [hidePassword, setHidePassword] = useState(true);
+  const [redirect, setRedirect] = useState("");
+
+  const handleClick = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // createNewAccount(email, password); //Change to sign in via firebase
+    setRedirect("/");
+  };
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const modalTitle = "Welcome to Employee Login";
+  const subtitle = "Sign in to continue";
+
+  if (redirect !== "") {
+    return <Redirect to={redirect} />;
+  }
+  return (
+    <CommonModal
+      title={modalTitle}
+      subtitle={subtitle}
+      show={show}
+      handleClose={handleClose}
+      body={
+        <div>
+          <form onSubmit={handleClick}>
+            <div>
+              <div className="text signup">Email address {}</div>
+              <input
+                name="email"
+                placeholder="Enter your company email"
+                type="text"
+                value={email}
+                className={errors.email ? "input-field error" : "input-field"}
+                onChange={onChangeEmail}
+              />
+            </div>
+            <div>
+              <div className="text signup">Password</div>
+              <div className="row bordered">
+                <input
+                  type={hidePassword ? "password" : "text"}
+                  name="password"
+                  className={
+                    errors.password
+                      ? "input-field error"
+                      : "input-field password"
+                  }
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={onChangePass}
+                />
+                <div
+                  className="input-field-btn"
+                  onClick={() => {
+                    setHidePassword(!hidePassword);
+                  }}
+                >
+                  {hidePassword ? <EyeFilled /> : <EyeInvisibleFilled />}
+                </div>
+              </div>
+            </div>
+            <div
+              className="text redirect right"
+              onClick={() => {
+                console.log("TODO: setRedirect('/password reset link')");
+              }}
+            >
+              Forgot your password?
+            </div>
+            <div style={{ height: 15 }} />
+            <button role="link" className="button">
+              Sign in
+            </button>
+            <div>
+              <div
+                className="text redirect center"
+                onClick={() => {
+                  setRedirect("/signup");
+                }}
+              >
+                <u>{"Don't have an account?"}</u>
+              </div>
+            </div>
+          </form>
+        </div>
+      }
+    />
+  );
+};
+
+export default SignInModal;
--- client/src/pages/SignUpModal.tsx
+++ client/src/pages/SignUpModal.tsx
@@ -1,151 +1,196 @@
-import './Modal.scss';
-import { OverlayTrigger, Popover } from 'react-bootstrap';
-import React, { FunctionComponent } from 'react';
-import CommonModal from '../components/organisms/Modal';
-import { createNewAccount } from '../services/auth';
-import { Redirect } from 'react-router-dom';
-import {useState} from 'react';
+import "./Modal.scss";
+import { OverlayTrigger, Popover } from "react-bootstrap";
+import React, { FunctionComponent } from "react";
+import CommonModal from "../components/organisms/Modal";
+import { createNewAccount } from "../services/auth";
+import { Redirect } from "react-router-dom";
+import { useState } from "react";
 
 const SignUpModal: FunctionComponent = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [hasOneLowerCase, setHasOneLowerCase] = useState(false);
-    const [hasOneUpperCase, setHasOneUpperCase] = useState(false);
-    const [hasOneNumber, setHasOneNumber] = useState(false);
-    const [hasOneSymbol, setHasOneSymbol] = useState(false);
-    const [hasTwelveCharacterMin, setHasTwelveCharacterMin] = useState(false);
-    const handleClose = () => setShow(false);
-    const initialReq : string[] = ["at least 1 lowercase letter", "at least 1 uppercase letter","at least 1 number","at least 1 symbol","12 characters minimum" ]
-    const [requirements, setRequirements] = useState(initialReq);
-    const [show, setShow] = useState(true);
-    const [redirectToHome, setRedirectToHome] = useState(false);
-    const requirementsAreFulfilled = !hasOneLowerCase || !hasOneUpperCase || !hasOneNumber || !hasOneSymbol || !hasTwelveCharacterMin;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [hasOneLowerCase, setHasOneLowerCase] = useState(false);
+  const [hasOneUpperCase, setHasOneUpperCase] = useState(false);
+  const [hasOneNumber, setHasOneNumber] = useState(false);
+  const [hasOneSymbol, setHasOneSymbol] = useState(false);
+  const [hasTwelveCharacterMin, setHasTwelveCharacterMin] = useState(false);
+  const handleClose = () => setShow(false);
+  const initialReq: string[] = [
+    "at least 1 lowercase letter",
+    "at least 1 uppercase letter",
+    "at least 1 number",
+    "at least 1 symbol",
+    "12 characters minimum",
+  ];
+  const [requirements, setRequirements] = useState(initialReq);
+  const [show, setShow] = useState(true);
+  const [errors, setErrors] = useState({ email: "", password: "" });
+  const [redirect, setRedirect] = useState("");
+  const requirementsAreFulfilled =
+    !hasOneLowerCase ||
+    !hasOneUpperCase ||
+    !hasOneNumber ||
+    !hasOneSymbol ||
+    !hasTwelveCharacterMin;
 
-    const handleClick = async (e: React.FormEvent<HTMLFormElement>) => { 
-        e.preventDefault();
-        createNewAccount(email, password);
-        setRedirectToHome(true);
-    };
+  const handleClick = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createNewAccount(email, password)
+      .then((result) => {
+        setErrors(result);
+        if (result.email === "" && result.password === "") {
+          setRedirect("/"); //TODO: change this to admin view endpoint
+        }
+      })
+      .catch((err) => {
+        setErrors(err);
+      });
+  };
 
-    const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    };
-     
-    const requirementToTestMap = new Map ([
-        ['lowerCase', /^(?=.*[a-z])/],
-        ['upperCase', /^(?=.*[A-Z])/],
-        ['number', /^(?=.*[0-9])/],
-        ['symbol', /^(?=.*[*!@#$%^&(){}[\]:;<>,.?/~_+\-=|\\])/],
-        ['twelveCharacters', /^(?=.{12,})/]
-    ]);
-        
-    const requirementToStateSetterMap = new Map ([
-        ['lowerCase', setHasOneLowerCase],
-        ['upperCase', setHasOneUpperCase],
-        ['number', setHasOneNumber],
-        ['symbol', setHasOneSymbol],
-        ['twelveCharacters', setHasTwelveCharacterMin],
-    ]);
-        
-    const requirementToMessageMap = new Map ([
-        ['lowerCase', "at least 1 lowercase letter"],
-        ['upperCase', "at least 1 uppercase letter"],
-        ['number', "at least 1 number"],
-        ['symbol', 'at least 1 symbol'],
-        ['twelveCharacters', '12 characters minimum'],
-    ]);
-          
-          
-    const onChangePass = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const password = e.target.value;
-        const copy: string[] = requirements;
-        
-        requirementToTestMap.forEach((test, key) => {
-            const result = test.test(password)!; 
-            const message : string = requirementToMessageMap.get(key)!;
-            requirementToStateSetterMap.get(key)!(result)
-            
-            // if requirement not in string and not in array, push into array
-            if(!result && !copy.includes(message)){
-                copy.push(message);
-            }
-            // if requirement is in the string and the array has it, remove it from array
-            else if(result && copy.includes(message)){
-                const index = copy.indexOf(message);
-                copy.splice(index, 1); 
-            }
-            setRequirements(copy);
-        });
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
-        setPassword(password);
-    }
+  const requirementToTestMap = new Map([
+    ["lowerCase", /^(?=.*[a-z])/],
+    ["upperCase", /^(?=.*[A-Z])/],
+    ["number", /^(?=.*[0-9])/],
+    ["symbol", /^(?=.*[*!@#$%^&(){}[\]:;<>,.?/~_+\-=|\\])/],
+    ["twelveCharacters", /^(?=.{12,})/],
+  ]);
 
-    const modalTitle = "Create Your Account";
-    const subtitle = "Register your email and create a password";
+  const requirementToStateSetterMap = new Map([
+    ["lowerCase", setHasOneLowerCase],
+    ["upperCase", setHasOneUpperCase],
+    ["number", setHasOneNumber],
+    ["symbol", setHasOneSymbol],
+    ["twelveCharacters", setHasTwelveCharacterMin],
+  ]);
 
-    const popover = (
-        <Popover id="popover-basic" show={requirementsAreFulfilled}>
-            <Popover.Title as="h3">Password Requirements</Popover.Title>
-            <Popover.Content>
-                {
-                    requirements.length > 0 ?  
-                    <ul>
-                        {
-                        requirements.map((i) => <li key={i}>{i}</li>)
-                        }
-                    </ul>
-                    :
-                    <div className="text signup">Your password meets the requirements!</div>   
-                }
-            </Popover.Content>
-        </Popover>
-    );
-    if (redirectToHome){
-        return (<Redirect to="/test"/>)
-    }
-    return (
-        <CommonModal title={modalTitle} subtitle={subtitle} show={show} handleClose={handleClose} body={
-            <div>
-                <form onSubmit={handleClick}>
-                
-                <div>
-                    <div className="text signup">
-                        Email address
-                    </div>
-                    <input
-                        name="email"
-                        placeholder="Enter your company email"
-                        type="text"
-                        value={email}
-                        className="input-field"
-                        onChange={onChangeEmail}
-                    />
-                </div>
-                <div>
-                    <div className="text signup">
-                        Password
-                    </div>
-                
-                    <div className="pass-req">
-                        <OverlayTrigger placement="bottom" overlay={popover}>
-                            <input
-                                type="password"
-                                name="password"
-                                className="input-field"
-                                placeholder="Enter your password"
-                                value={password}
-                                onChange={onChangePass}           
-                            />
-                        </OverlayTrigger>
-                    </div>
-                </div>
-                <button role="link" className="button" disabled={requirementsAreFulfilled}>
-                    Sign Up
-                </button>
-            </form>
-        </div>}/>
-    );
+  const requirementToMessageMap = new Map([
+    ["lowerCase", "at least 1 lowercase letter"],
+    ["upperCase", "at least 1 uppercase letter"],
+    ["number", "at least 1 number"],
+    ["symbol", "at least 1 symbol"],
+    ["twelveCharacters", "12 characters minimum"],
+  ]);
+
+  const onChangePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const password = e.target.value;
+    const copy: string[] = requirements;
+
+    requirementToTestMap.forEach((test, key) => {
+      const result = test.test(password)!;
+      const message: string = requirementToMessageMap.get(key)!;
+      requirementToStateSetterMap.get(key)!(result);
+
+      // if requirement not in string and not in array, push into array
+      if (!result && !copy.includes(message)) {
+        copy.push(message);
+      }
+      // if requirement is in the string and the array has it, remove it from array
+      else if (result && copy.includes(message)) {
+        const index = copy.indexOf(message);
+        copy.splice(index, 1);
+      }
+      setRequirements(copy);
+    });
+
+    setPassword(password);
+  };
+
+  const modalTitle = "Create Your Account";
+  const subtitle = "Register your email and create a password";
+
+  const popover = (
+    <Popover id="popover-basic" show={requirementsAreFulfilled}>
+      <Popover.Title as="h3">Password Requirements</Popover.Title>
+      <Popover.Content>
+        {requirements.length > 0 ? (
+          <ul>
+            {requirements.map((i) => (
+              <li key={i}>{i}</li>
+            ))}
+          </ul>
+        ) : (
+          <div className="text signup">
+            Your password meets the requirements!
+          </div>
+        )}
+      </Popover.Content>
+    </Popover>
+  );
+  if (redirect !== "") {
+    return <Redirect to={redirect} />;
   }
-  
-export default SignUpModal;
+  return (
+    <CommonModal
+      title={modalTitle}
+      subtitle={subtitle}
+      show={show}
+      handleClose={handleClose}
+      body={
+        <div>
+          <form onSubmit={handleClick}>
+            <div>
+              <div className="row">
+                <div className="text signup">Email Address</div>
+                <div className="text error">{errors.email}</div>
+              </div>
+              <input
+                name="email"
+                placeholder="Enter your company email"
+                type="text"
+                value={email}
+                className={errors.email ? "input-field error" : "input-field"}
+                onChange={onChangeEmail}
+              />
+            </div>
+            <div>
+              <div className="row">
+                <div className="text signup">
+                  Password<div>{errors.password}</div>
+                </div>
+              </div>
 
+              <div className="pass-req">
+                <OverlayTrigger placement="bottom" overlay={popover}>
+                  <input
+                    type="password"
+                    name="password"
+                    className={
+                      errors.password ? "input-field error" : "input-field"
+                    }
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={onChangePass}
+                  />
+                </OverlayTrigger>
+              </div>
+            </div>
+            <div style={{ height: 40 }} />
+            <button
+              role="link"
+              className="button"
+              disabled={requirementsAreFulfilled}
+            >
+              Sign Up
+            </button>
+            <div>
+              <div
+                onClick={() => {
+                  setRedirect("/login");
+                }}
+                className="text redirect center"
+              >
+                <u>{"Have an account?"}</u>
+              </div>
+            </div>
+          </form>
+        </div>
+      }
+    />
+  );
+};
+
+export default SignUpModal;
