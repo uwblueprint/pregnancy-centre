1f60e270a8e088f73bd2353a0ae8291b0007b52a
Guy Romm <guy.romm@outlook.com>
2021-03-21 16:35:39 -0400

    added most fields for table

--- client/src/components/molecules/RequestGroupTable.tsx
+++ client/src/components/molecules/RequestGroupTable.tsx
@@ -1,45 +1,56 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Table } from "react-bootstrap";
 import moment from 'moment';
 
-import RequestGroupListItem from '../atoms/RequestGroupListItem';
+import Tag from '../atoms/Tag'
 
 import RequestGroup from '../../data/types/requestGroup'
-import requestsReducer from "../../data/reducers/requestsReducer";
 
 export interface Props {
   requestGroups: Array<RequestGroup>
 }
 
 const RequestGroupTable: FunctionComponent<Props> = (props: Props) => {
-  return <div className="request-group-table">
-    <Table hover>
-        <thead>
-            <tr>
-                <th>Title (A-Z)</th>
-                <th>Open Requests</th>
-                <th>Item Types</th>
-                <th>Next Recipient</th>
-                <th>Last Added</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            {props.requestGroups.map((requestGroup : RequestGroup) => {
+    const [selectedRequestGroup, setSelectedRequestGroup] = useState("");
+
+    return <div className="request-group-table">
+        <Table>
+            <thead>
                 <tr>
-                    <td>{ requestGroup.name }</td>
-                    <td>{ requestGroup.numOpen }</td>
-                    <td>TODO</td>
-                    <td>TODO</td>
-                    <td>{ moment(requestGroup.dateUpdated, "x").format('MMMM DD, YYYY') }</td>
-                    <td><img src={ requestGroup.image }/></td>
+                    <th>Title (A-Z)</th>
+                    <th>Open Requests</th>
+                    <th>Item Types</th>
+                    <th>Next Recipient</th>
+                    <th>Last Added</th>
+                    <th></th>
                 </tr>
-            })}
-        </tbody>
-    </Table>
-    {props.requestGroups.length == 0 && <span>There are no request groups. <a>Create one now</a></span>
-    }
-  </div>
+            </thead>
+            <tbody>
+                {props.requestGroups.map((requestGroup : RequestGroup) => {
+                    <tr key={ requestGroup._id } onClick={ () => { requestGroup._id ? setSelectedRequestGroup(requestGroup._id) : setSelectedRequestGroup("") } }>
+                        <td>{ requestGroup.name }{ selectedRequestGroup === requestGroup._id && <span className="edit-icon"/> }</td>
+                        <td>{ requestGroup.numOpen }</td>
+                        <td>
+                            <div className="requestType-tag-list">
+                                { requestGroup.requestTypes && requestGroup.requestTypes.map((requestType) => {
+                                    if (requestType.name) {
+                                        return(<Tag text={requestType.name}/>)
+                                    } else {
+                                        return
+                                    }
+                                })}
+                            </div>
+                        </td>
+                        <td>TODO</td>
+                        <td>{ moment(requestGroup.dateUpdated, "x").format('MMMM DD, YYYY') }</td>
+                        <td><img src={ requestGroup.image }/></td>
+                    </tr>
+                })}
+            </tbody>
+        </Table>
+        {props.requestGroups.length == 0 && <span>There are no request groups. <a>Create one now</a></span>
+        }
+    </div>
 };
 
 export default RequestGroupTable;
