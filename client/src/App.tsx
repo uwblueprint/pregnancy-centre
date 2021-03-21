2f9977438f621b7b56dc76d7702af53722db7429
jlight99 <ellenhuang523@gmail.com>
2021-03-21 15:47:19 -0400

    add tag atom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,14 +18,11 @@ function App(): JSX.Element {
       <Switch>
         
         <Route path='/signup' strict component={SignUpModal}></Route>
-        <Route path='/confirmation' component={() => <ConfirmationModal email="anna@pregnancycentre.ca"/>}></Route>
         <Route path='/donation-guidelines'><SampleContainer /></Route>
         <Route path='/login'><SampleContainer /></Route>
         {/* TODO(jlight99): delete /confirmation endpoint after logic for triggering the confirmation modal upon signup has been added */}
         <Route path='/confirmation' component={() => <ConfirmationModal email="anna@pregnancycentre.ca"/>}></Route>
         <Route path='/signup' strict component={SignUpModal}></Route>
-        {/* TODO(jlight99): delete /email-confirmed endpoint after logic for triggering the email confirmed modal has been added */}
-        <Route path='/email-confirmed' strict component={EmailConfirmedModal}></Route>
         <Route path='/verify-email' component={AuthTestComponent}></Route>
         <Route path='/password-reset-email-sent' strict component={PasswordResetEmailSentModal}></Route>
         <Route path='/test'>
