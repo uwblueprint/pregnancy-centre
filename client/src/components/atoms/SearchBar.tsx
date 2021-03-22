1ab868049fc127c08d0558352e02f8237c9e752d
jlight99 <ellenhuang523@gmail.com>
2021-03-22 10:38:58 -0400

    add search bar atom

---
+++ client/src/components/atoms/SearchBar.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent, useState } from "react";
+
+interface Props {
+  defaultText: string,
+  onSearchStringChange: React.ChangeEventHandler<HTMLInputElement>,
+}
+
+const SearchBar: FunctionComponent<Props> = (props: Props) => {
+  const [searchString, setSearchString] = useState("");
+
+  const onSearchStringChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchString(event.target.value);
+    props.onSearchStringChange(event);
+  }
+
+  return <span>
+    <form>
+      <input className="search-bar" type="text" placeholder={props.defaultText} value={searchString} onChange={onSearchStringChange} />
+    </form>
+  </span>
+};
+
+export default SearchBar;
