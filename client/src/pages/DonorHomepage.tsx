d055f3094791228792d613898c78a3df57a2d9bb
jlight99 <ellenhuang523@gmail.com>
2021-03-22 11:41:52 -0400

    remove test code from donor homepage

--- client/src/pages/DonorHomepage.tsx
+++ client/src/pages/DonorHomepage.tsx
@@ -1,24 +1,16 @@
 import React, { FunctionComponent } from "react";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import SearchBar from "../components/atoms/SearchBar";
 
 import DonorPage from '../components/layouts/DonorPage'
 import DonorRequestGroupBrowser from '../components/organisms/DonorRequestGroupBrowser'
 import HomepageBanner from '../components/organisms/HomepageBanner'
-import EmailConfirmedModal from "./EmailConfirmedModal";
 
 const DonorHomepage: FunctionComponent = () => {
-    const onSearchStringChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log("yay!");
-        console.log(event.target.value);
-    }
-
   return <Container className="donor-homepage" fluid>
     <DonorPage>
       <Row><HomepageBanner /></Row>
       <Row className="donor-homepage-request-groups-browser"><DonorRequestGroupBrowser /></Row>
-      <SearchBar defaultText="Search for a group..." onSearchStringChange={onSearchStringChange}></SearchBar>
     </DonorPage>
   </Container>;
 };
