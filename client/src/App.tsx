5e47c83f0c64241f064071f672c200a4a728a7ec
chamod-gamage <chamodgamage26@gmail.com>
2021-03-21 20:06:30 -0400

    unreformat App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,47 +16,20 @@ function App(): JSX.Element {
   return (
     <span>
       <Router>
-        <Switch>
-          <Route path="/signup" strict component={SignUpModal}></Route>
-          <Route
-            path="/confirmation"
-            component={() => (
-              <ConfirmationModal email="anna@pregnancycentre.ca" />
-            )}
-          ></Route>
-          <Route path="/donation-guidelines">
-            <SampleContainer />
-          </Route>
-          {/* TODO(chamod-gamage): confirm what the routing behaviour will be for user signin sequence */}
-          <Route path="/login">
-            <SignInModal />
-          </Route>
-          {/* TODO(jlight99): delete /confirmation endpoint after logic for triggering the confirmation modal upon signup has been added */}
-          <Route
-            path="/confirmation"
-            component={() => (
-              <ConfirmationModal email="anna@pregnancycentre.ca" />
-            )}
-          ></Route>
-          <Route path="/signup" strict component={SignUpModal}></Route>
-          {/* TODO(jlight99): delete /email-confirmed endpoint after logic for triggering the email confirmed modal has been added */}
-          <Route
-            path="/email-confirmed"
-            strict
-            component={EmailConfirmedModal}
-          ></Route>
-          <Route path="/verify-email" component={AuthTestComponent}></Route>
-          <Route
-            path="/password-reset-email-sent"
-            strict
-            component={PasswordResetEmailSentModal}
-          ></Route>
-          <Route path="/test">
-            <RequestGroupDonorView requestGroupId="603d9b41eb57fc06447b8a23" />
-          </Route>
-          <Route path="/">
-            <DonorHomepage />
-          </Route>
+      <Switch>
+        <Route path='/signup' strict component={SignUpModal}></Route>
+        <Route path='/donation-guidelines'><SampleContainer /></Route>
+        {/* TODO(chamod-gamage): confirm what the routing behaviour will be for user signin sequence */}
+        <Route path='/login'><SignInModal /></Route>
+        {/* TODO(jlight99): delete /confirmation endpoint after logic for triggering the confirmation modal upon signup has been added */}
+        <Route path='/confirmation' component={() => <ConfirmationModal email="anna@pregnancycentre.ca"/>}></Route>
+        <Route path='/signup' strict component={SignUpModal}></Route>
+        <Route path='/verify-email' component={AuthTestComponent}></Route>
+        <Route path='/password-reset-email-sent' strict component={PasswordResetEmailSentModal}></Route>
+        <Route path='/test'>
+          <RequestGroupDonorView requestGroupId="603d9b41eb57fc06447b8a23"/>
+        </Route>
+        <Route path='/'><DonorHomepage /></Route>
         </Switch>
       </Router>
     </span>
