991e1ca47aee127af819cba2139dc1647d1e6086
anandissac <anand.z.issac@gmail.com>
2021-03-23 13:45:02 -0400

    changes

--- client/src/components/molecules/RequestTypeDropdown.tsx
+++ client/src/components/molecules/RequestTypeDropdown.tsx
@@ -11,8 +11,11 @@ interface Props {
 
 const RequestTypeDropdown: FunctionComponent<Props> = (props: Props) => {
     const [requestType, setRequestType] = useState(props.requestType);
+    const [backupRequestType, setBackupRequestType] = useState(requestType);
     const [editModalShow, setEditModalShow] = useState(false);
     const [deleteModalShow, setDeleteModalShow] = useState(false);
+    const [isTooLong, setIsTooLong] = useState(false);
+
     const dummyRequests = [{"_id": "605570e1acc0254a485e44c4","fulfilled": false,"client": {"firstName": "Willie",
     "lastName": "Franecki"},"dateCreated": "1590740228592"}, {"_id": "605570e1acc0254a485e44c6","fulfilled": false,"client": {"firstName": "Emmie","lastName": "Bernier"},"dateCreated": "1564965009667"}];
     
@@ -20,13 +23,24 @@ const RequestTypeDropdown: FunctionComponent<Props> = (props: Props) => {
     const deleteModalTitle = "Delete Type";
     const deleteModalSubtitle = "Are you sure you want to delete “250 ML” as a type in the group “Bottles”? This will delete all 20 requests within this type and cannot be undone."
     
-    const handleEditModalClose = () => setEditModalShow(false);
+    const tooLongMessage = "Type name cannot exceed 50 characters!";
+
+    const handleEditModalClose = () => {
+        setRequestType(backupRequestType);
+        setIsTooLong(false);
+        setEditModalShow(false)
+    };
     const handleDeleteModalClose = () => setDeleteModalShow(false);
 
     const handleClick = async (e: React.FormEvent<HTMLFormElement>) => { 
         e.preventDefault();
         // change requestType here 
+        setBackupRequestType(requestType);
     };
+
+    const deleteRequestType = async () => {
+        //delete requestType here
+    }
 
 
     // when user clicks edit button request type
@@ -34,13 +48,20 @@ const RequestTypeDropdown: FunctionComponent<Props> = (props: Props) => {
         setEditModalShow(true);
     }
 
+    // when user clicks delete button request type
     const onOpenDeleteRequestType = () => {
         setDeleteModalShow(true);
     }
 
     // user editing request type in modal
     const onEditRequestType = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setRequestType(e.target.value);
+        const newRequestType = e.target.value;
+        if (newRequestType.length > 50 ){
+            setIsTooLong(true);
+        } else {
+            setIsTooLong(false);
+            setRequestType(e.target.value);
+        }
     };
 
     return (
@@ -48,13 +69,14 @@ const RequestTypeDropdown: FunctionComponent<Props> = (props: Props) => {
             <Dropdown >
                 <Dropdown.Toggle className="request-type-dropdown" id="dropdown-basic">
                     <div className="type-text">
-                        {props.requestType}
+                        {requestType}
                     </div>
-                    <div className="button-container">
+                    
+                </Dropdown.Toggle>
+                <div className="button-container">
                         <a className="button-container edit" onClick={onOpenEditRequestType}><FaPen/></a>
                         <a className="button-container delete" onClick={onOpenDeleteRequestType}><FaTrashAlt/></a>
-                    </div>
-                </Dropdown.Toggle>
+                </div>
                 
                 <Dropdown.Menu>
                     <Dropdown.ItemText><RequestsTable requests={dummyRequests} /></Dropdown.ItemText>
@@ -73,6 +95,11 @@ const RequestTypeDropdown: FunctionComponent<Props> = (props: Props) => {
                                 onChange={onEditRequestType}
                             />
                         </div>
+                        {isTooLong && 
+                        <div className="too-long-container">
+                            <h4 className="too-long-text">{tooLongMessage}</h4>
+                        </div>
+                        }
                         <button role="link" className="button" >
                             Save Changes
                         </button>
@@ -81,7 +108,7 @@ const RequestTypeDropdown: FunctionComponent<Props> = (props: Props) => {
             }/>
             <CommonModal title={deleteModalTitle} subtitle={deleteModalSubtitle} handleClose={handleDeleteModalClose} show={deleteModalShow} body={
                 <div>
-                    <button role="link" className="button" >
+                    <button role="link" className="button" onClick={deleteRequestType}>
                         Confirm
                     </button>
                 </div>
