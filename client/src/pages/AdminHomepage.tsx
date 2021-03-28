5ba99c50ab6af39268d1d9e902fdcded8d0f2ac7
jlight99 <ellenhuang523@gmail.com>
2021-03-28 16:54:13 -0400

    add dummy admin homepage

---
+++ client/src/pages/AdminHomepage.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+const AdminHomepage: FunctionComponent = () => {
+
+  return (
+    <div>
+        I am the admin homepage
+    </div>
+  );
+}
+
+export default AdminHomepage;
