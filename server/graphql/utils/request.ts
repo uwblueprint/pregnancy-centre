b9d9494eed13e2fa5c6c0fc5505e3d912bfbabc0
kevin <kevinwang2040@gmail.com>
2021-03-29 19:31:05 -0400

    Add better error handling

--- server/graphql/utils/request.ts
+++ server/graphql/utils/request.ts
@@ -1,6 +1,5 @@
 import { Document } from 'mongoose'
 import { RequestInterface } from '../../models/requestModel'
-import { ServerResponseInterface } from '../serverResponse'
 import { UserInputError } from 'apollo-server-errors'
 
 import { updateRequestTypeHelper } from './requestType'
@@ -25,38 +24,55 @@ const updateRequestHelper = (request, dataSources): Promise<Document> => {
   if(!request.id) {
     throw new UserInputError('Missing argument value', { argumentName: 'id' })
   }
-  if(request.fulfilled) {
+
+  const currentRequestCopy = dataSources.requests.getById(request.id.toString()).toObject()
+  currentRequestCopy.id = currentRequestCopy._id
+  const currentRequest = dataSources.requests.getById(request.id.toString())
+  let oldRequestTypeCopy, newRequestTypeCopy
+
+  if(request.requestType) {
+    // If these fail then nothing has been changed at this point, so nothing to revert
+    oldRequestTypeCopy = dataSources.requestTypes.getById(currentRequest.requestType.toString()).toObject()
+    oldRequestTypeCopy.id = oldRequestTypeCopy._id
+    newRequestTypeCopy = dataSources.requestTypes.getById(request.requestType.toString()).toObject()
+    newRequestTypeCopy.id = newRequestTypeCopy._id
+  }
+
+  if(request.fulfilled === true) {
     request.dateFulfilled = Date.now()
   }
-  const currentRequest = dataSources.requests.getById(request.id.toString())
+
   if(request.requestType) {
+    const oldRequestType = dataSources.requestTypes.getById(currentRequest.requestType.toString())
+    const newRequestType = dataSources.requestTypes.getById(request.requestType.toString())
+    oldRequestType.requests = oldRequestType.requests.filter(id => !id.equals(request.id))
+    newRequestType.requests.push(request.id)
+    dataSources.requestTypes.update(oldRequestType)
+    dataSources.requestTypes.update(newRequestType)
 
-    // If these throw, then there is nothing to revert and error should be returned to Apollo
-    const originalPreviousRequestType = dataSources.requestTypes.getById(currentRequest.requestType.toString())
-    const originalNewRequestType = dataSources.requestTypes.getById(request.requestType.toString())
-    try {
-      const previousRequestType = dataSources.requestTypes.getById(currentRequest.requestType.toString())
-      const newRequestType = dataSources.requestTypes.getById(request.requestType.toString())
-      previousRequestType.requests = previousRequestType.requests.filter(item => {
-        return !item.equals(request.id)
-      })
-      newRequestType.requests.push(currentRequest._id)
-      dataSources.requestTypes.update(newRequestType)
-      dataSources.requestTypes.update(previousRequestType)
-      
-      
-    }
-    catch(error) {
-      console.log(error)
-    }
-
+    currentRequest.requestType = request.requestType
   }
+
   const requestTypeId = currentRequest.requestType
   return dataSources.requests.update(request)
     .then(res => {
       updateRequestTypeHelper({"id": requestTypeId}, dataSources)
       return res
     })
+    .catch(error => {
+      console.log(error);
+
+      // Reverting to original copies
+      dataSources.requests.update(currentRequestCopy)
+      if(request.requestType) {
+        dataSources.requestTypes.update(oldRequestTypeCopy)
+        dataSources.requestTypes.update(newRequestTypeCopy)
+      }
+      throw error
+
+    })
+
+
 }
 
 export { filterDeletedRequests, filterOpenRequests, filterFulfilledRequests, getRequestsById, updateRequestHelper }
