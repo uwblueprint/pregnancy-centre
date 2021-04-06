1417df59224250a1e443b1e157d75c7d678d83f4
kevin <kevinwang2040@gmail.com>
2021-04-05 21:12:11 -0400

    Use bootstrap icons

--- client/src/components/layouts/AdminPage.tsx
+++ client/src/components/layouts/AdminPage.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from "react";
 import Footer from '../organisms/Footer'
 import Navbar from '../organisms/Navbar'
 
-const DonorPage: FunctionComponent<Record<string, unknown>> = (props: React.PropsWithChildren<Record<string, unknown>>) => {
+const AdminPage: FunctionComponent<Record<string, unknown>> = (props: React.PropsWithChildren<Record<string, unknown>>) => {
   return <div className="donor-page">
     <div className="donor-page-header">
       <Navbar links={ [ { name: "Donation Hub", link: "https://pregnancycentre.ca/"  }, 
@@ -18,4 +18,4 @@ const DonorPage: FunctionComponent<Record<string, unknown>> = (props: React.Prop
     </div>
 };
 
-export default DonorPage
+export default AdminPage
