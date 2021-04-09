46d2dc771246f780da6352d2e255c9384166c175
chamod-gamage <chamodgamage26@gmail.com>
2021-04-09 14:48:32 -0400

    imagelist atom (gotta clean up stuff)

---
+++ client/src/components/atoms/ImageList.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from "react";
+import Row from "react-bootstrap/Row";
+import ScrollWindow from "./ScrollWindow";
+
+interface Props {
+  onImageChange(): void;
+  images: string[];
+}
+
+const ImageList: FunctionComponent<Props> = (props: Props) => {
+  return (
+    <>
+      <br />
+      <div className="imagelist-container">
+        <ScrollWindow noBorder>
+          <Row noGutters className="imagelist img-grid">
+            {props.images.map((url: string) => {
+              return (
+                <div className="imagelist img-wrapper col-3" key={url}>
+                  <img
+                    onClick={props.onImageChange}
+                    className="imagelist img-wrapper img"
+                    src={url}
+                  />
+                </div>
+              );
+            })}
+          </Row>
+        </ScrollWindow>
+      </div>
+    </>
+  );
+};
+
+export default ImageList;
--- client/src/components/atoms/ScrollWindow.tsx
+++ client/src/components/atoms/ScrollWindow.tsx
@@ -1,13 +1,17 @@
 import React, { FunctionComponent } from "react";
 
 interface Props {
-  children: React.ReactNode
+  children: React.ReactNode;
+  noBorder?: boolean;
 }
 
 const ScrollWindow: FunctionComponent<Props> = (props: Props) => {
-  return <div className="scroll-window">
-    {props.children}
-  </div>
+  return (
+    <div className={`scroll-window${props.noBorder && "-no-border"}`}>
+      {console.log(props.noBorder)}
+      {props.children}
+    </div>
+  );
 };
 
 export default ScrollWindow;
