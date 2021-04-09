aa3fb459ebfec22edc16f95174d45fe80e7ed1ac
chamod-gamage <chamodgamage26@gmail.com>
2021-04-09 18:39:18 -0400

    pretty much everything for imagepicker atom

---
+++ client/src/components/atoms/ImagePicker.tsx
@@ -0,0 +1,41 @@
+import React, { FunctionComponent } from "react";
+import ImageList from "./ImageList";
+import Row from "react-bootstrap/Row";
+
+interface Props {
+  onImageChange(url: string): void;
+  images: string[];
+  selected: string;
+  isErroneous: boolean;
+}
+
+const ImagePicker: FunctionComponent<Props> = (props: Props) => {
+  const { selected, images, onImageChange, isErroneous } = props;
+  return (
+    <div className="imagepicker" style={{ margin: 40, width: 350 }}>
+      <Row className="toprow">
+        <div className="imagepicker-title">Image</div>
+        {isErroneous && (
+          <div className="text-error">Please select an image</div>
+        )}
+      </Row>
+      {isErroneous && <i className="bi bi-exclamation-circle alert-icon"></i>}
+      <div className={`imagepicker-preview`}>
+        {selected.length ? (
+          <img src={selected} />
+        ) : (
+          <div className={isErroneous ? "error" : "unselected"}>
+            Select an image
+          </div>
+        )}
+      </div>
+      <ImageList
+        selected={selected}
+        images={images}
+        onImageChange={onImageChange}
+      />
+    </div>
+  );
+};
+
+export default ImagePicker;
