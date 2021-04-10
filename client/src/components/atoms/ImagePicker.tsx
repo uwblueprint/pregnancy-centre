5476dae6a9f0f427967e3a82f8a52452de705203
chamod-gamage <chamodgamage26@gmail.com>
2021-04-09 23:31:50 -0400

    remove title and error message

--- client/src/components/atoms/ImagePicker.tsx
+++ client/src/components/atoms/ImagePicker.tsx
@@ -13,12 +13,6 @@ const ImagePicker: FunctionComponent<Props> = (props: Props) => {
   const { selected, images, onImageChange, isErroneous } = props;
   return (
     <div className="imagepicker">
-      <Row className="toprow">
-        <div className="imagepicker-title">Image</div>
-        {isErroneous && !selected.length && (
-          <div className="text-error">Please select an image</div>
-        )}
-      </Row>
       {isErroneous && <i className="bi bi-exclamation-circle alert-icon"></i>}
       <div className={`imagepicker-preview`}>
         {selected.length ? (
