52bebac3a74015e91e689cb42177806c71452abc
Bonnie Chin <bonniechin.mail@gmail.com>
2021-04-10 08:42:15 -0400

    bare minimum layout

--- client/src/pages/ResetPasswordModal.tsx
+++ client/src/pages/ResetPasswordModal.tsx
@@ -137,7 +137,6 @@ const ResetPasswordModal: FunctionComponent = () => {
                       value={password}
                       onChange={onChangePass}
                     />
-                    
                 </OverlayTrigger>
               </div>
             </div>
