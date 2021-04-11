a6d6ae286045839e3a35227a8510df1c859c9828
kevin <kevinwang2040@gmail.com>
2021-04-10 21:42:16 -0400

    Add styling

--- client/src/components/molecules/FormItem.tsx
+++ client/src/components/molecules/FormItem.tsx
@@ -16,15 +16,16 @@ const FormItem: FunctionComponent<Props> = (props: Props) => {
     const isError = props.errorString.length > 0
     return (
         <div className="form-item">
-            <h1>Hi this is pregnancy centre</h1>
+            <h1>pregnancy centre</h1>
             <div className="form-item-top">
-                <span>
+                <span className={props.isDisabled ? "form-item-disabled" : undefined}>
                     {props.formItemName}
                     { props.tooltipText ? <Tooltip tooltipText={props.tooltipText}/> : null}
                 </span>
-                <span className="form-item-error-text">Error string</span>
+                {isError && <span className="form-item-error-text">{props.errorString}</span>}
             </div>
             {props.inputComponent}
+            {isError && <i className="form-item-error-icon bi bi-exclamation-circle alert-icon"></i>}
         </div>
     )
 }
