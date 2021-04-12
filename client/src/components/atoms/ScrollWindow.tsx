ecdc44f572b4b0284e2e7974ffbc0e8453f793a1
Megan Niu <megan.niu@gmail.com>
2021-04-12 11:31:07 -0400

    Use custom scrollbar lib

--- client/src/components/atoms/ScrollWindow.tsx
+++ client/src/components/atoms/ScrollWindow.tsx
@@ -1,12 +1,17 @@
+import 'simplebar';
+import 'simplebar/dist/simplebar.css';
 import React, { FunctionComponent } from "react";
+// import SimpleBar from 'simplebar-react';
+
+// import 'simplebar/dist/simplebar.min.css';
 
 interface Props {
   children: React.ReactNode
 }
 
 const ScrollWindow: FunctionComponent<Props> = (props: Props) => {
-  return <div className="scroll-window">
-    {props.children}
+  return <div className="scroll-window" data-simplebar>
+      {props.children}
   </div>
 };
 
