28031a9587857e5faeaa0ddcf915b4db5a9e15ee
kevin <kevinwang2040@gmail.com>
2021-04-13 19:02:50 -0400

    Remove unused style

--- client/src/components/layouts/AdminPage.tsx
+++ client/src/components/layouts/AdminPage.tsx
@@ -4,10 +4,8 @@ import Navbar from '../organisms/Navbar'
 
 const AdminPage: FunctionComponent<Record<string, unknown>> = (props: React.PropsWithChildren<Record<string, unknown>>) => {
   return <div className="donor-page">
-    <div className="donor-page-header">
       <Navbar links={ [ { name: "Donation Hub", link: "https://pregnancycentre.ca/"  }, 
                         { name: "Logout", link: "/"  } ] } />
-    </div>
     <div className="donor-page-content" >
       {props.children}
     </div>
