9730e36784d46129616610a4f15a0c6346411448
Megan Niu <megan.niu@gmail.com>
2021-04-12 21:46:17 -0400

    Remove error icon from imagePicker

--- client/src/components/atoms/ImagePicker.tsx
+++ client/src/components/atoms/ImagePicker.tsx
@@ -13,7 +13,6 @@ const ImagePicker: FunctionComponent<Props> = (props: Props) => {
   const { selected, images, onImageChange, isErroneous } = props;
   return (
     <div className="imagepicker">
-      {isErroneous && <i className="bi bi-exclamation-circle alert-icon"></i>}
       <div className={`imagepicker-preview`}>
         {selected.length ? (
           <img src={selected} />
