54b770e67ce71298cc58015b3e98ccb515b31f55
Megan Niu <megan.niu@gmail.com>
2021-04-14 17:30:27 -0400

    Add container and path for edit request form

---
+++ client/src/components/examples/EditRequestFormContainer.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent, useState } from "react";
+
+import RequestForm from "../organisms/RequestForm";
+
+
+const EditRequestGroupFormContainer: FunctionComponent<Record<string, never>> = () => {
+  const [show, setShow] = useState(true);
+
+  return (<>{show && <RequestForm handleClose={() => setShow(false)} operation="edit" requestId="6077592acca67d9812f94df5" />}</>)
+};
+
+export default EditRequestGroupFormContainer
+
