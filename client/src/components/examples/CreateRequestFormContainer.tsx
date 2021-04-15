53f0bac96ce1acb8b4972528961a2cdb65ed8e34
Guy Romm <guy.romm@outlook.com>
2021-04-15 14:52:16 -0400

    reload on submit as appropriate

--- client/src/components/examples/CreateRequestFormContainer.tsx
+++ client/src/components/examples/CreateRequestFormContainer.tsx
@@ -3,11 +3,11 @@ import React, { FunctionComponent, useState } from "react";
 import RequestForm from "../organisms/RequestForm";
 
 
-const CreateRequestGroupFormContainer: FunctionComponent<Record<string, never>> = () => {
+const CreateRequestFormContainer: FunctionComponent<Record<string, never>> = () => {
   const [show, setShow] = useState(true);
 
-  return (<>{show && <RequestForm handleClose={() => setShow(false)} operation="create" />}</>)
+  return (<>{show && <RequestForm onSubmitComplete={() => { window.location.reload() }} handleClose={() => setShow(false)} operation="create" />}</>)
 };
 
-export default CreateRequestGroupFormContainer
+export default CreateRequestFormContainer
 
