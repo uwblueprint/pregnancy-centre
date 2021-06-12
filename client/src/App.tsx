61b4fb69ba819ddb10cc9dfe37be96003046b782
Megan Niu <megan.niu@gmail.com>
2021-06-12 19:00:26 -0400

    Create KW Map

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,8 +10,6 @@ import SendResetPasswordEmailModal from "./pages/SendResetPasswordEmailModal";
 import SignInModal from "./pages/SignInModal";
 import SignUpModal from "./pages/SignUpModal";
 
-import DonationItemCardContainer from "./components/examples/DonationItemCardContainer";
-
 function App(): JSX.Element {
   return (
     <span>
@@ -30,9 +28,6 @@ function App(): JSX.Element {
           <Route path="/admin" component={AdminHomepage}></Route>
           <Route path="/request-group/:id">
             <AdminRequestGroupView />
-          </Route>
-          <Route path="/test">
-            <DonationItemCardContainer />
           </Route>
           <Route path="/">
             <DonorHomepage />
---
+++ client/src/data/types/donorHomepageConfig.ts
@@ -0,0 +1,10 @@
+export interface Point {
+  id: number,
+  testimonial: string,
+  x: number,
+  y: number
+}
+
+export interface DonorHompageConfig {
+  Map: Array<Point>
+}
