0dac54fc4576a7e28f80fabae011634778890ab0
kevin <kevinwang2040@gmail.com>
2021-06-14 17:31:12 -0400

    Run linter on server

--- server/server.ts
+++ server/server.ts
@@ -11,19 +11,14 @@ import { getUser } from "./auth/firebase";
 import inflate from "inflation";
 import raw from "raw-body";
 
-import {
-  ClientCache,
-  RequestCache,
-  RequestGroupCache,
-  RequestTypeCache,
-} from "./database/cache";
+import { ClientCache, RequestCache, RequestGroupCache, RequestTypeCache } from "./database/cache";
 import ClientDataSource from "./datasources/clientDataSource";
 import RequestDataSource from "./datasources/requestDataSource";
 import RequestGroupDataSource from "./datasources/requestGroupDataSource";
 import RequestTypeDataSource from "./datasources/requestTypeDataSource";
 import { resolvers } from "./graphql/resolvers";
 import { typeDefs } from "./graphql/schema";
-import User from "./auth/user"
+import User from "./auth/user";
 
 // TODO: need to make script to build(compile) prod server and to run prod server
 
@@ -35,8 +30,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const isProd = process.env.NODE_ENV !== "dev";
 const corsPolicy = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
+    origin: process.env.CLIENT_URL,
+    credentials: true
 };
 
 // -----------------------------------------------------------------------------
@@ -45,10 +40,10 @@ const corsPolicy = {
 
 // connect to MongoDB and setup data sources
 connectDB(() => {
-  ClientCache.init();
-  RequestCache.init();
-  RequestTypeCache.init();
-  RequestGroupCache.init();
+    ClientCache.init();
+    RequestCache.init();
+    RequestTypeCache.init();
+    RequestGroupCache.init();
 });
 
 // -----------------------------------------------------------------------------
@@ -56,97 +51,93 @@ connectDB(() => {
 // -----------------------------------------------------------------------------
 
 async function gqlServer() {
-  const app = express();
-  app.use(cookieParser());
-  app.use(
-    "/sessionLogin",
-    bodyParser.json({ strict: true, type: "application/json" })
-  );
-  app.use(cors(corsPolicy));
+    const app = express();
+    app.use(cookieParser());
+    app.use("/sessionLogin", bodyParser.json({ strict: true, type: "application/json" }));
+    app.use(cors(corsPolicy));
 
-  //custom body parser for apolloClient GraphQL queries using CreateHTTPLink because express.json() is buggy
-  app.use("/graphql", async (req, res, next) => {
-    if (
-      req &&
-      req.headers &&
-      req.headers["content-type"] &&
-      (req.headers["content-type"] === "application/graphql" ||
-        req.headers["content-type"].includes("application/graphql"))
-    ) {
-      const str = await raw(inflate(req), { encoding: "utf8" });
-      req.body = JSON.parse(str);
-    }
-    await next();
-  });
-
-  app.post("/sessionLogin", (req, res) => {
-    // Get the ID token passed
-    const idToken = req.body.idToken.toString();
-    // Set session expiration to 5 days.
-    const expiresIn = 60 * 60 * 24 * 5 * 1000;
-    // Create the session cookie. This will also verify the ID token in the process.
-    // The session cookie will have the same claims as the ID token.
-    // To only allow session cookie setting on recent sign-in, auth_time in ID token
-    // can be checked to ensure user was recently signed in before creating a session cookie.
-    admin
-      .auth()
-      .createSessionCookie(idToken, { expiresIn })
-      .then(
-        (sessionCookie) => {
-          // Set cookie policy for session cookie.
-          const options = {
-            maxAge: expiresIn,
-            httpOnly: true,
-            secure: true,
-          };
-          res.cookie("session", sessionCookie, options);
-          res.end(JSON.stringify({ status: "success" }));
-        },
-        (error) => {
-          res.status(401).send(error);
+    //custom body parser for apolloClient GraphQL queries using CreateHTTPLink because express.json() is buggy
+    app.use("/graphql", async (req, res, next) => {
+        if (
+            req &&
+            req.headers &&
+            req.headers["content-type"] &&
+            (req.headers["content-type"] === "application/graphql" ||
+                req.headers["content-type"].includes("application/graphql"))
+        ) {
+            const str = await raw(inflate(req), { encoding: "utf8" });
+            req.body = JSON.parse(str);
         }
-      );
-  });
+        await next();
+    });
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req, res }) => ({
-      authenticateUser: async () => {
-        if (!isProd) {
-          return { req, res, user: null };
-        }
+    app.post("/sessionLogin", (req, res) => {
+        // Get the ID token passed
+        const idToken = req.body.idToken.toString();
+        // Set session expiration to 5 days.
+        const expiresIn = 60 * 60 * 24 * 5 * 1000;
+        // Create the session cookie. This will also verify the ID token in the process.
+        // The session cookie will have the same claims as the ID token.
+        // To only allow session cookie setting on recent sign-in, auth_time in ID token
+        // can be checked to ensure user was recently signed in before creating a session cookie.
+        admin
+            .auth()
+            .createSessionCookie(idToken, { expiresIn })
+            .then(
+                (sessionCookie) => {
+                    // Set cookie policy for session cookie.
+                    const options = {
+                        maxAge: expiresIn,
+                        httpOnly: true,
+                        secure: true
+                    };
+                    res.cookie("session", sessionCookie, options);
+                    res.end(JSON.stringify({ status: "success" }));
+                },
+                (error) => {
+                    res.status(401).send(error);
+                }
+            );
+    });
 
-        if (!req.cookies || !req.cookies.session) {
-          throw new AuthenticationError("Authentication Not Found");
-        }
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: async ({ req, res }) => ({
+            authenticateUser: async () => {
+                if (!isProd) {
+                    return { req, res, user: null };
+                }
 
-        const user = await getUser(req.cookies.session);
-        if (!user || !user.id)
-          throw new AuthenticationError("Authentication Error");
+                if (!req.cookies || !req.cookies.session) {
+                    throw new AuthenticationError("Authentication Not Found");
+                }
 
-        return { req, res, user };
-      }
-    }),
-    dataSources: () => ({
-      clients: new ClientDataSource(),
-      requests: new RequestDataSource(),
-      requestTypes: new RequestTypeDataSource(),
-      requestGroups: new RequestGroupDataSource(),
-    }),
-  });
+                const user = await getUser(req.cookies.session);
+                if (!user || !user.id) throw new AuthenticationError("Authentication Error");
 
-  server.applyMiddleware({
-    app,
-    path: "/graphql",
-    bodyParserConfig: { strict: true, type: "application/*" },
-    cors: corsPolicy,
-  });
+                return { req, res, user };
+            }
+        }),
+        dataSources: () => ({
+            clients: new ClientDataSource(),
+            requests: new RequestDataSource(),
+            requestTypes: new RequestTypeDataSource(),
+            requestGroups: new RequestGroupDataSource()
+        })
+    });
 
-  app.listen({ port: PORT });
-  console.log(`🚀 Server ready at port ${PORT}`);
+    server.applyMiddleware({
+        app,
+        path: "/graphql",
+        bodyParserConfig: { strict: true, type: "application/*" },
+        cors: corsPolicy
+    });
 
-  return { server, app };
+    app.listen({ port: PORT });
+    console.log(`🚀 Server ready at port ${PORT}`);
+
+    return { server, app };
 }
 
 gqlServer();
