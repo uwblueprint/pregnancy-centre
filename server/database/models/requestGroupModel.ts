04303bd88b5f16bf917e2f02c35f3696bfb347d4
Philips Xu <philips.ilips@gmail.com>
2021-06-26 16:13:15 -0400

    updated request group schema to include reference to donation groups

--- server/database/models/requestGroupModel.ts
+++ server/database/models/requestGroupModel.ts
@@ -1,68 +1,81 @@
 import { Document, model, Schema, Types } from "mongoose";
 
 interface RequestTypeEmbeddingInterface {
-    _id: Types.ObjectId;
+  _id: Types.ObjectId;
 }
-
+interface DonationGroupEmbeddingInterface {
+  _id: Types.ObjectId;
+}
 interface RequestGroupInterface extends Document {
-    _id: Types.ObjectId;
+  _id: Types.ObjectId;
 
-    // Properties
-    name: string;
-    description: string;
-    image: string;
+  // Properties
+  name: string;
+  description: string;
+  image: string;
 
-    // Embedded Objects
-    requestTypes: Array<RequestTypeEmbeddingInterface>;
+  // Embedded Objects
+  requestTypes: Array<RequestTypeEmbeddingInterface>;
+  donationGroups: Array<DonationGroupEmbeddingInterface>;
 
-    // Timestamps for Statuses
-    deletedAt: Date;
+  // Timestamps for Statuses
+  deletedAt: Date;
 
-    // Timestamps
-    createdAt: Date;
-    updatedAt: Date;
+  // Timestamps
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const RequestGroupSchema = new Schema(
-    {
-        // Properties
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        description: {
-            type: String,
-            required: true,
-            default: ""
-        },
-        image: {
-            type: String,
-            required: true,
-            default: ""
-        },
+  {
+    // Properties
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    image: {
+      type: String,
+      required: true,
+      default: "",
+    },
 
-        // Embedded Objects
-        requestTypes: {
-            type: [
-                {
-                    // @ts-ignore
-                    _id: { type: Types.ObjectId, ref: "RequestType" }
-                }
-            ],
-            default: []
+    // Embedded Objects
+    requestTypes: {
+      type: [
+        {
+          _id: { type: Types.ObjectId, ref: "RequestType" },
         },
+      ],
+      default: [],
+    },
+    donationGroups: {
+      type: [
+        {
+          _id: { type: Types.ObjectId, ref: "DonationGroup" },
+        },
+      ],
+      default: [],
+    },
 
-        // Timestamps for Statuses
-        deletedAt: {
-            type: Date
-        }
-    }, // Options
-    {
-        timestamps: true
-    }
+    // Timestamps for Statuses
+    deletedAt: {
+      type: Date,
+    },
+  }, // Options
+  {
+    timestamps: true,
+  }
 );
 
-const RequestGroup = model<RequestGroupInterface>("RequestGroup", RequestGroupSchema);
+const RequestGroup = model<RequestGroupInterface>(
+  "RequestGroup",
+  RequestGroupSchema
+);
 
 export { RequestGroup, RequestGroupInterface, RequestTypeEmbeddingInterface };
