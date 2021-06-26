960f5e5cfc1f62d0d4ab0bb008061fb431b8007e
Philips Xu <philips.ilips@gmail.com>
2021-06-26 16:13:48 -0400

    updated request model to include reference to donation groups

--- server/database/models/requestModel.ts
+++ server/database/models/requestModel.ts
@@ -1,54 +1,67 @@
 import { Document, model, Schema, Types } from "mongoose";
 
+interface DonationGroupEmbeddingInterface {
+  _id: Types.ObjectId;
+}
+
 interface RequestInterface extends Document {
-    _id: Types.ObjectId;
+  _id: Types.ObjectId;
 
-    // Properties
-    quantity: number;
-    clientName: String;
+  // Properties
+  quantity: number;
+  clientName: string;
 
-    // References
-    requestType: Types.ObjectId;
+  // References
+  requestType: Types.ObjectId;
+  donationGroups: Array<DonationGroupEmbeddingInterface>;
 
-    // Timestamps
-    createdAt: Date;
-    updatedAt: Date;
+  // Timestamps
+  createdAt: Date;
+  updatedAt: Date;
 
-    // Timestamps for Statuses
-    deletedAt: Date;
-    fulfilledAt: Date;
+  // Timestamps for Statuses
+  deletedAt: Date;
+  fulfilledAt: Date;
 }
 
 const requestSchema = new Schema(
-    {
-        // Properties
-        quantity: {
-            type: Number,
-            required: true,
-            default: 1
-        },
-        clientName: {
-            type: String,
-            required: true
-        },
+  {
+    // Properties
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    clientName: {
+      type: String,
+      required: true,
+    },
 
-        // References
-        requestType: {
-            type: Types.ObjectId,
-            ref: "RequestType"
+    // References
+    requestType: {
+      type: Types.ObjectId,
+      ref: "RequestType",
+    },
+    donationGroups: {
+      type: [
+        {
+          _id: { type: Types.ObjectId, ref: "DonationGroup" },
         },
+      ],
+      default: [],
+    },
 
-        // Timestamps for Statuses
-        deletedAt: {
-            type: Date
-        },
-        fulfilledAt: {
-            type: Date
-        }
-    }, // Options
-    {
-        timestamps: true
-    }
+    // Timestamps for Statuses
+    deletedAt: {
+      type: Date,
+    },
+    fulfilledAt: {
+      type: Date,
+    },
+  }, // Options
+  {
+    timestamps: true,
+  }
 );
 
 const Request = model<RequestInterface>("Request", requestSchema);
