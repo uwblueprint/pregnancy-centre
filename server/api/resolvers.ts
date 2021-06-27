802bad72d0cfae95cf57e1b0492b1cb8320eaf04
anandissac <anand.z.issac@gmail.com>
2021-06-26 21:34:57 -0400

    added donation form resolvers

--- server/api/resolvers.ts
+++ server/api/resolvers.ts
@@ -1,20 +1,14 @@
-import {
-    requestGroupMutationResolvers,
-    requestGroupQueryResolvers,
-    requestGroupResolvers
-} from "./resolvers/requestGroupResolvers";
-import { requestMutationResolvers, requestQueryResolvers, requestResolvers } from "./resolvers/requestResolvers";
-import {
-    requestTypeMutationResolvers,
-    requestTypeQueryResolvers,
-    requestTypeResolvers
-} from "./resolvers/requestTypeResolvers";
+import { requestGroupMutationResolvers, requestGroupQueryResolvers, requestGroupResolvers } from './resolvers/requestGroupResolvers'
+import { requestMutationResolvers, requestQueryResolvers, requestResolvers } from './resolvers/requestResolvers'
+import { requestTypeMutationResolvers, requestTypeQueryResolvers, requestTypeResolvers } from './resolvers/requestTypeResolvers'
+import { donationFormQueryResolvers } from './resolvers/donationFormResolvers'
 
 const resolvers = {
     Query: {
         ...requestQueryResolvers,
         ...requestTypeQueryResolvers,
-        ...requestGroupQueryResolvers
+        ...requestGroupQueryResolvers,
+        ...donationFormQueryResolvers
     },
 
     Mutation: {
