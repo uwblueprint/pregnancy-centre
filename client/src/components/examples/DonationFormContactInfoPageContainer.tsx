4dc3807e3d401e0a3836404ac8a6d94431e33d38
Megan Niu <megan.niu@gmail.com>
2021-07-02 18:46:51 -0400

    Add DonationFormContactInfoPage

---
+++ client/src/components/examples/DonationFormContactInfoPageContainer.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+import DonationFormContactInfoPage from "../organisms/DonationFormContactInfoPage";
+
+const DonationFormContactInfoPageContainer: FunctionComponent<
+  Record<string, never>
+> = () => {
+  return <DonationFormContactInfoPage onNext={(donor) => console.log(donor)} />;
+};
+
+export default DonationFormContactInfoPageContainer;
