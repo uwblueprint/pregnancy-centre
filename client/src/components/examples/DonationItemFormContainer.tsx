b4b7e326fb4d6c11c09d0c642a073c083ebb03b0
Megan Niu <megan.niu@gmail.com>
2021-07-02 12:03:29 -0400

    Add more examples to item form demo container

--- client/src/components/examples/DonationItemFormContainer.tsx
+++ client/src/components/examples/DonationItemFormContainer.tsx
@@ -12,22 +12,46 @@ const DonationItemFormContainer: FunctionComponent<Record<string, never>> =
     ];
 
     return (
-      <div
-        style={{
-          marginTop: "30px",
-          marginLeft: "30px",
-          display: "flex",
-          flexDirection: "column",
-          width: "70%",
-        }}
-      >
-        <DonationItemForm
-          onDelete={() => console.log("Delete")}
-          onSave={(donationForm) => console.log(donationForm)}
-          requestGroups={requestGroups}
-          showFormUnsavedError={false}
-        />
-      </div>
+      <>
+        <h1 style={{ marginBottom: "50px" }}>Donation Item Form</h1>
+        <div
+          style={{
+            marginTop: "30px",
+            marginLeft: "30px",
+            marginBottom: "50px",
+            display: "flex",
+            flexDirection: "column",
+            width: "70%",
+          }}
+        >
+          <DonationItemForm
+            onDelete={() => console.log("Delete")}
+            onSave={(donationForm) => console.log(donationForm)}
+            requestGroups={requestGroups}
+            showFormUnsavedError={false}
+          />
+        </div>
+        <h1 style={{ marginBottom: "50px" }}>
+          Donation Item Form with unsaved form error
+        </h1>
+        <div
+          style={{
+            marginTop: "30px",
+            marginLeft: "30px",
+            marginBottom: "50px",
+            display: "flex",
+            flexDirection: "column",
+            width: "70%",
+          }}
+        >
+          <DonationItemForm
+            onDelete={() => console.log("Delete")}
+            onSave={(donationForm) => console.log(donationForm)}
+            requestGroups={requestGroups}
+            showFormUnsavedError={true}
+          />
+        </div>
+      </>
     );
   };
 
