21f8ea79460d9ec3790269dc5d78c1008f9fddd7
Megan Niu <megan.niu@gmail.com>
2021-07-02 19:07:14 -0400

    Run prettier

--- client/src/components/examples/StepperContainer.tsx
+++ client/src/components/examples/StepperContainer.tsx
@@ -3,12 +3,12 @@ import React, { FunctionComponent } from "react";
 import Stepper from "../atoms/Stepper";
 
 const StepperContainer: FunctionComponent<Record<string, never>> = () => {
-  const steps = ["Contact Information", "Item Details", "Review and Confirm"];
-  return (
-    <div style={{ marginTop: "30px", marginLeft: "30px" }}>
-      <Stepper steps={steps} selectedStep={0} />
-    </div>
-  );
+    const steps = ["Contact Information", "Item Details", "Review and Confirm"];
+    return (
+        <div style={{ marginTop: "30px", marginLeft: "30px" }}>
+            <Stepper steps={steps} selectedStep={0} />
+        </div>
+    );
 };
 
 export default StepperContainer;
