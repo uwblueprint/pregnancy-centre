f80b5ff5c0a1b4fe74a89dfc5ecda6d1f447781a
Megan Niu <megan.niu@gmail.com>
2021-07-03 11:56:36 -0400

    Add previous/next page buttons to DonorFormPage

--- client/src/components/examples/DonationFormContactInfoPageContainer.tsx
+++ client/src/components/examples/DonationFormContactInfoPageContainer.tsx
@@ -4,7 +4,7 @@ import DonationFormContactInfoPage from "../../pages/DonationFormContactInfoPage
 
 const DonationFormContactInfoPageContainer: FunctionComponent<Record<string, never>> = () => {
     const steps = ["Contact Information", "Item Details", "Review and Confirm"];
-    return <DonationFormContactInfoPage onNext={(donor) => console.log(donor)} steps={steps} />;
+    return <DonationFormContactInfoPage onNext={(donor) => console.log(donor)} pageNumber={1} steps={steps} />;
 };
 
 export default DonationFormContactInfoPageContainer;
