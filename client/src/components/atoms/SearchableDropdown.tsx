3c73a3f2090258497bc291c184af2ff4a2715a98
Megan Niu <megan.niu@gmail.com>
2021-07-02 20:13:10 -0400

    Fix lint error

--- client/src/components/atoms/SearchableDropdown.tsx
+++ client/src/components/atoms/SearchableDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import ScrollableDropdown from "../atoms/ScrollableDropdown";
 import Tag from "../atoms/Tag";
