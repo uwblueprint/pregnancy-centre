fb153c6fd89f0a04bcc65110e5c92238c4e58dba
Bonnie Chin <bonniechin.mail@gmail.com>
2021-07-03 22:03:09 -0400

    finished carousel??? :scream:

---
+++ client/src/components/atoms/BannerCarousel.tsx
@@ -0,0 +1,21 @@
+import {Carousel, Container} from 'react-bootstrap';
+import React, { FunctionComponent } from "react";
+import DonorHomepageConfig from '../../config/donorHompageConfig.json';
+
+
+const BannerCarousel: FunctionComponent = () => {
+    const bannerImgs = DonorHomepageConfig.Banner;
+    const bannerIntv = 1000 * 10;
+    return <Container fluid>
+        <Carousel controls={false} interval={bannerIntv} indicators={false} className="banner-carousel">
+            {bannerImgs.map((imgLink, index) => (
+                <Carousel.Item key={index}>
+                    <img className="d-block w-100 banner-carousel" src={`${imgLink}`}/>
+                </Carousel.Item>
+                )
+            )}
+        </Carousel>
+    </Container>
+};
+
+export default BannerCarousel;
--- client/src/components/organisms/HomepageBanner.tsx
+++ client/src/components/organisms/HomepageBanner.tsx
@@ -1,44 +1,15 @@
 import React, { FunctionComponent } from "react";
+import BannerCarousel from "../atoms/BannerCarousel";
 
-import BannerPicture from "../../assets/homepage-banner.svg";
-// import PacifierIcon from '../../assets/pacifier-icon.svg'
-// import RubberDuckIcon from '../../assets/rubber-duck-icon.svg'
-// import StrollerIcon from '../../assets/stroller-icon.svg'
-
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 
 const HomepageBanner: FunctionComponent = () => {
-    return (
-        <div className="homepage-banner">
-            <Row className="justify-content-between">
-                <Col className="align-self-center homepage-banner-info" md={5}>
-                    <h1>Help women and families in Kitchener-Waterloo thrive with your donation today</h1>
-                    <p style={{ width: "80%" }}>Scroll to see our clients&#39; current needs and arrange a donation</p>
-                    {/* <Row className="homepage-banner-stats">
-          <Col className="homepage-banner-stat">
-            <img className="homepage-banner-icon" src={StrollerIcon} />
-            <h2>80+</h2>
-            <p>regular donors</p>
-          </Col>
-          <Col className="homepage-banner-stat">
-            <img className="homepage-banner-icon" src={PacifierIcon} />
-            <h2>566</h2>
-            <p>families helped</p>
-          </Col>
-          <Col className="homepage-banner-stat">
-            <img className="homepage-banner-icon" src={RubberDuckIcon} />
-            <h2>2932</h2>
-            <p>items donated</p>
-          </Col>
-        </Row> */}
-                </Col>
-                <Col className="homepage-banner-image-container" md={5}>
-                    <img className="homepage-banner-image" src={BannerPicture} />
-                </Col>
-            </Row>
-        </div>
-    );
+  return <div className="d-flex justify-content-center homepage-banner" >
+    <BannerCarousel/>
+    <div className="col-md-6 homepage-banner-info">
+      <h1>Help women and families in Kitchener-Waterloo thrive with your donation today</h1>
+      <p>Scroll to see our clients&#39; current needs and arrange a donation</p>
+    </div>
+  </div>
 };
 
 export default HomepageBanner;
