d7dbbd702228b8e067b063d488ab7c320ddb640e
Megan Niu <megan.niu@gmail.com>
2021-07-06 23:22:35 -0400

    Run prettier

--- client/src/components/atoms/CardWithShadow.tsx
+++ client/src/components/atoms/CardWithShadow.tsx
@@ -1,13 +1,11 @@
 import React, { FunctionComponent } from "react";
 
 interface Props {
-  children: React.ReactNode
+    children: React.ReactNode;
 }
 
 const CardWithShadow: FunctionComponent<Props> = (props: Props) => {
-  return <div className="card-with-shadow">
-    {props.children}
-  </div>
+    return <div className="card-with-shadow">{props.children}</div>;
 };
 
 export default CardWithShadow;
--- client/src/components/organisms/DonorImpactSection.tsx
+++ client/src/components/organisms/DonorImpactSection.tsx
@@ -10,87 +10,79 @@ import Cursor from "../../assets/cursor.png";
 import DonorHomepageConfig from "../../config/donorHompageConfig.json";
 
 const DonorImpactSection: FunctionComponent = () => {
-  const [selectedTestimonial, _setSelectedTestimonial] =
-    useState<Testimonial | null>(null);
-  const testimonials = DonorHomepageConfig.map.testimonials;
-  const setSelectedTestimonial = (id: number | null) => {
-    if (!id) {
-      _setSelectedTestimonial(null);
-      return;
-    }
-    const testimonial = testimonials.find(
-      (testimonial: Testimonial) => testimonial.id === id
+    const [selectedTestimonial, _setSelectedTestimonial] = useState<Testimonial | null>(null);
+    const testimonials = DonorHomepageConfig.map.testimonials;
+    const setSelectedTestimonial = (id: number | null) => {
+        if (!id) {
+            _setSelectedTestimonial(null);
+            return;
+        }
+        const testimonial = testimonials.find((testimonial: Testimonial) => testimonial.id === id);
+        _setSelectedTestimonial(testimonial ?? null);
+    };
+    const points = DonorHomepageConfig.map.points;
+    const markerSize =
+        testimonials.length > 0 && testimonials.length <= DonorHomepageConfig.map.markerSizes.length
+            ? DonorHomepageConfig.map.markerSizes[testimonials.length - 1]
+            : DonorHomepageConfig.map.defaultMarkerSize;
+    const maxPoints = points.length;
+    const testimonialPoints: Array<MapWithMarkersPoint> = testimonials.reduce(
+        (accumulator: Array<MapWithMarkersPoint>, testimonial: Testimonial, index) => {
+            if (index >= maxPoints) {
+                return accumulator;
+            }
+            accumulator.push({
+                id: testimonial.id,
+                imagePath: testimonial.imagePath,
+                x: points[index].x,
+                y: points[index].y
+            });
+            return accumulator;
+        },
+        []
     );
-    _setSelectedTestimonial(testimonial ?? null);
-  };
-  const points = DonorHomepageConfig.map.points;
-  const markerSize =
-    testimonials.length > 0 &&
-    testimonials.length <= DonorHomepageConfig.map.markerSizes.length
-      ? DonorHomepageConfig.map.markerSizes[testimonials.length - 1]
-      : DonorHomepageConfig.map.defaultMarkerSize;
-  const maxPoints = points.length;
-  const testimonialPoints: Array<MapWithMarkersPoint> = testimonials.reduce(
-    (
-      accumulator: Array<MapWithMarkersPoint>,
-      testimonial: Testimonial,
-      index
-    ) => {
-      if (index >= maxPoints) {
-        return accumulator;
-      }
-      accumulator.push({
-        id: testimonial.id,
-        imagePath: testimonial.imagePath,
-        x: points[index].x,
-        y: points[index].y,
-      });
-      return accumulator;
-    },
-    []
-  );
 
-  return (
-    <div className="donor-impact-section">
-      <h1 className="donor-impact-section-header">Our Impact</h1>
-      <Row className="donor-impact-section-content">
-        <Col className="map-section" sm={0} xl={1} />
-        <Col className="map-section" sm={12} xl={5}>
-          <div className="instructions">
-            <img src={Cursor} />
-            <p>hover over the images for some client testimonials!</p>
-          </div>
-          <MapWithMarkers
-            baseMapPath={BaseMap}
-            markerSize={markerSize}
-            points={testimonialPoints}
-            onEnterHoverOverMarker={setSelectedTestimonial}
-            onLeaveHoverOverMarker={() => setSelectedTestimonial(null)}
-          />
-        </Col>
-        <Col className="info-section" sm={12} xl={5}>
-          <div className="stats-section">
-            {DonorHomepageConfig.statistics.map((stat: Statistic) => (
-              <div className="stat" key={stat.icon}>
-                <div className="stat-top">
-                  <i className={stat.icon} />
-                  <h1>{stat.measurement}</h1>
-                </div>
-                <p>{stat.stat}</p>
-              </div>
-            ))}
-          </div>
-          {selectedTestimonial && (
-            <Card>
-              <img src={selectedTestimonial.imagePath} />
-              <p>{selectedTestimonial.testimonial}</p>
-            </Card>
-          )}
-        </Col>
-        <Col className="map-section" xs={0} xl={1} />
-      </Row>
-    </div>
-  );
+    return (
+        <div className="donor-impact-section">
+            <h1 className="donor-impact-section-header">Our Impact</h1>
+            <Row className="donor-impact-section-content">
+                <Col className="map-section" sm={0} xl={1} />
+                <Col className="map-section" sm={12} xl={5}>
+                    <div className="instructions">
+                        <img src={Cursor} />
+                        <p>hover over the images for some client testimonials!</p>
+                    </div>
+                    <MapWithMarkers
+                        baseMapPath={BaseMap}
+                        markerSize={markerSize}
+                        points={testimonialPoints}
+                        onEnterHoverOverMarker={setSelectedTestimonial}
+                        onLeaveHoverOverMarker={() => setSelectedTestimonial(null)}
+                    />
+                </Col>
+                <Col className="info-section" sm={12} xl={5}>
+                    <div className="stats-section">
+                        {DonorHomepageConfig.statistics.map((stat: Statistic) => (
+                            <div className="stat" key={stat.icon}>
+                                <div className="stat-top">
+                                    <i className={stat.icon} />
+                                    <h1>{stat.measurement}</h1>
+                                </div>
+                                <p>{stat.stat}</p>
+                            </div>
+                        ))}
+                    </div>
+                    {selectedTestimonial && (
+                        <Card>
+                            <img src={selectedTestimonial.imagePath} />
+                            <p>{selectedTestimonial.testimonial}</p>
+                        </Card>
+                    )}
+                </Col>
+                <Col className="map-section" xs={0} xl={1} />
+            </Row>
+        </div>
+    );
 };
 
 export default DonorImpactSection;
