ec21eb6608f746e4790f507c40b46641082ec228
Bonnie Chin <bonniechin.mail@gmail.com>
2021-07-07 13:41:55 -0400

    fixed layering issues caused by fade

--- client/src/components/atoms/BannerCarousel.tsx
+++ client/src/components/atoms/BannerCarousel.tsx
@@ -7,7 +7,7 @@ const BannerCarousel: FunctionComponent = () => {
     const bannerImgs = DonorHomepageConfig.Banner;
     const bannerIntv = 1000 * 10;
     return <Container fluid>
-        <Carousel controls={false} interval={bannerIntv} indicators={false} className="banner-carousel">
+        <Carousel controls={false} interval={bannerIntv} indicators={false} className="banner-carousel" fade>
             {bannerImgs.map((imgLink, index) => (
                 <Carousel.Item key={index}>
                     <img className="d-block w-100 banner-carousel" src={`${imgLink}`}/>
