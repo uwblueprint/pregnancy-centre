cf725889aa8421657382eba78d5bcc6b63d9c1be
Megan Niu <megan.niu@gmail.com>
2021-07-09 23:00:33 -0400

    Add styling

---
+++ client/src/components/examples/DonationFormProgressStepperContainer.tsx
@@ -0,0 +1,21 @@
+import React, { FunctionComponent, useState } from "react";
+
+import DonationFormProgressStepper from "../atoms/DonationFormProgressStepper";
+import { DonationItemStatus } from "../../data/types/donationForm";
+
+const DonationFormProgressStepperContainer: FunctionComponent<Record<string, never>> = () => {
+    const [status, setStatus] = useState(DonationItemStatus.PENDING_APPROVAL);
+    return (
+        <div
+            style={{
+                marginTop: "30px",
+                marginLeft: "30px",
+                width: "190px"
+            }}
+        >
+            <DonationFormProgressStepper status={status} onStatusChange={setStatus} />
+        </div>
+    );
+};
+
+export default DonationFormProgressStepperContainer;
