9be4333dec3dbbafeeaaa1c2f3a19fda927aee21
Megan Niu <megan.niu@gmail.com>
2021-07-10 04:00:35 -0400

    Fix date formatting and data validation

--- client/src/components/atoms/DonationFormMatchingCard.tsx
+++ client/src/components/atoms/DonationFormMatchingCard.tsx
@@ -14,23 +14,20 @@ const DonationFormMatchingCard: FunctionComponent<DonationFormMatchingCardProps>
     props: DonationFormMatchingCardProps
 ) => {
     const { donationForm, onSelectMatch, onViewForm } = props;
+    const contactFullName = [donationForm?.contact?.firstName, donationForm?.contact?.lastName]
+        .filter((name) => name)
+        .join(" ");
     return (
         <div className="donation-form-matching-card">
             <div className="header">
                 <div className="header-text">
                     <h1>{`${donationForm.quantityRemaining}/${donationForm.quantity} items available`}</h1>
-                    <h2>{moment(donationForm.createdAt).format("MMM D, YYYY, h:mm \a")}</h2>
+                    <h2>{moment(donationForm.createdAt).format("MMM D, YYYY, h:mma")}</h2>
                 </div>
                 <Button text="Match" copyText="" onClick={onSelectMatch} />
             </div>
-            {(donationForm?.contact?.firstName || donationForm?.contact?.lastName) && (
-                <h2>{`Donated by: ${donationForm?.contact?.firstName ?? ""} ${
-                    donationForm?.contact?.lastName ?? ""
-                }`}</h2>
-            )}
-            {donationForm.adminNotes && donationForm.adminNotes.length !== 0 && (
-                <h2>{`Notes: ${donationForm.adminNotes}`}</h2>
-            )}
+            {contactFullName && <h2>{`Donated by: ${contactFullName}`}</h2>}
+            {donationForm.adminNotes && <h2>{`Notes: ${donationForm.adminNotes}`}</h2>}
             <h3 onClick={onViewForm}>View original form</h3>
         </div>
     );
