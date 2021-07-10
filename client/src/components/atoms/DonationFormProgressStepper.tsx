f2437621ce01d9d9d93f997f7ede3a4d247fe9bd
Megan Niu <megan.niu@gmail.com>
2021-07-09 21:45:49 -0400

    Create DonationFormProgressStepper

---
+++ client/src/components/atoms/DonationFormProgressStepper.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent, useState } from "react";
+
+interface Props {
+    
+}
+
+const DonationFormProgressStepper: FunctionComponent<Props> = (props: Props) => {
+    
+};
+
+export default DonationFormProgressStepper;
