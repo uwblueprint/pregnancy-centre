ff021a3529d933b6f1c90f5b398f2251481a5500
Megan Niu <megan.niu@gmail.com>
2021-07-10 04:32:44 -0400

    Simplify example container

--- client/src/components/examples/DonationFormMatchingCardContainer.tsx
+++ client/src/components/examples/DonationFormMatchingCardContainer.tsx
@@ -5,16 +5,7 @@ import DonationFormMatchingCard from "../atoms/DonationFormMatchingCard";
 
 const DonationFormMatchingCardContainer: FunctionComponent<Record<string, never>> = () => {
     const donationForm: DonationForm = {
-        adminNotes: "This bottle feeds lots of milk, you will be very happy with this bottle!",
-        contact: {
-            firstName: "Miley",
-            lastName: "Cyrus"
-        },
-        createdAt: new Date(),
-        quantity: 5,
-        quantityRemaining: 4
-    };
-    const donationFormWithoutNotes: DonationForm = {
+        // adminNotes: "This bottle feeds lots of milk, you will be very happy with this bottle!",
         contact: {
             firstName: "Miley",
             lastName: "Cyrus"
@@ -33,15 +24,8 @@ const DonationFormMatchingCardContainer: FunctionComponent<Record<string, never>
                 width: "42%"
             }}
         >
-            <h3 style={{ marginTop: "30px" }}>Donation form with notes</h3>
             <DonationFormMatchingCard
                 donationForm={donationForm}
-                onSelectMatch={() => console.log("Select Match")}
-                onViewForm={() => console.log("View Form")}
-            />
-            <h3 style={{ marginTop: "30px" }}>Donation form without notes</h3>
-            <DonationFormMatchingCard
-                donationForm={donationFormWithoutNotes}
                 onSelectMatch={() => console.log("Select Match")}
                 onViewForm={() => console.log("View Form")}
             />
