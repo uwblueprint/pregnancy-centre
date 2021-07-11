22e1f221d7b2ad9b1dff7e42992fdf606674b5f3
Bonnie Chin <bonniechin.mail@gmail.com>
2021-07-10 20:54:52 -0400

    prettified files

--- client/src/components/organisms/HomepageBanner.tsx
+++ client/src/components/organisms/HomepageBanner.tsx
@@ -1,15 +1,16 @@
 import React, { FunctionComponent } from "react";
 import BannerCarousel from "../atoms/BannerCarousel";
 
-
 const HomepageBanner: FunctionComponent = () => {
-  return <div className="d-flex justify-content-center homepage-banner" >
-    <BannerCarousel/>
-    <div className="col-md-6 homepage-banner-info">
-      <h1>Help women and families in Kitchener-Waterloo thrive with your donation today</h1>
-      <p>Scroll to see our clients&#39; current needs and arrange a donation</p>
-    </div>
-  </div>
+    return (
+        <div className="d-flex justify-content-center homepage-banner">
+            <BannerCarousel />
+            <div className="col-md-6 homepage-banner-info">
+                <h1>Help women and families in Kitchener-Waterloo thrive with your donation today</h1>
+                <p>Scroll to see our clients&#39; current needs and arrange a donation</p>
+            </div>
+        </div>
+    );
 };
 
 export default HomepageBanner;
