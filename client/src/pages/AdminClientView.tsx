3c77e6cdd41e7744640fdc1ec32e6e4ef6e8b762
Bonnie Chin <bonniechin.mail@gmail.com>
2021-07-10 20:54:52 -0400

    prettified files

--- client/src/pages/AdminClientView.tsx
+++ client/src/pages/AdminClientView.tsx
@@ -1,16 +1,17 @@
 import React, { FunctionComponent } from "react";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
-import AdminPage from '../components/layouts/AdminPage'
-import AdminRequestClientBrowser from '../components/organisms/AdminRequestClientBrowser';
-import ClientRequestTable from '../components/molecules/ClientRequestTable'
+import AdminPage from "../components/layouts/AdminPage";
+import AdminRequestClientBrowser from "../components/organisms/AdminRequestClientBrowser";
+import ClientRequestTable from "../components/molecules/ClientRequestTable";
 
 const AdminRequestGroupView: FunctionComponent = () => {
-  return <Container className="admin-homepage" fluid>
-    <AdminPage>
-    </AdminPage>
-  </Container>;
+    return (
+        <Container className="admin-homepage" fluid>
+            <AdminPage></AdminPage>
+        </Container>
+    );
 };
 
 export default AdminRequestGroupView;
