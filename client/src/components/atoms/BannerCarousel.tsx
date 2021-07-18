c91b7e7db9c812841ff907ef98b433a6abbf0bac
Bonnie Chin <bonniechin.mail@gmail.com>
2021-07-17 20:28:08 -0400

    moved content into just the atom

--- client/src/components/atoms/BannerCarousel.tsx
+++ client/src/components/atoms/BannerCarousel.tsx
@@ -6,15 +6,21 @@ const BannerCarousel: FunctionComponent = () => {
     const bannerImgs = DonorHomepageConfig.Banner;
     const bannerIntv = 1000 * 10;
     return (
-        <Container fluid>
-            <Carousel controls={false} interval={bannerIntv} indicators={false} className="banner-carousel" fade>
-                {bannerImgs.map((imgLink, index) => (
-                    <Carousel.Item key={index}>
-                        <img className="d-block w-100 banner-carousel" src={`${imgLink}`} />
-                    </Carousel.Item>
-                ))}
-            </Carousel>
-        </Container>
+        <div className="homepage-banner">
+            <Container fluid>
+                <Carousel controls={false} interval={bannerIntv} indicators={false} className="banner-carousel" fade>
+                    {bannerImgs.map((imgLink, index) => (
+                        <Carousel.Item key={index}>
+                            <img className="banner-carousel" src={`${imgLink}`} />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            </Container>
+            <div className="col-md-6 homepage-banner-info">
+                <h1>Help women and families in Kitchener-Waterloo thrive with your donation today</h1>
+                <p>Scroll to see our clients&#39; current needs and arrange a donation</p>
+            </div>
+        </div>
     );
 };
 
--- client/src/components/organisms/HomepageBanner.tsx
+++ client/src/components/organisms/HomepageBanner.tsx
@@ -3,13 +3,7 @@ import BannerCarousel from "../atoms/BannerCarousel";
 
 const HomepageBanner: FunctionComponent = () => {
     return (
-        <div className="d-flex justify-content-center homepage-banner">
-            <BannerCarousel />
-            <div className="col-md-6 homepage-banner-info">
-                <h1>Help women and families in Kitchener-Waterloo thrive with your donation today</h1>
-                <p>Scroll to see our clients&#39; current needs and arrange a donation</p>
-            </div>
-        </div>
+        <BannerCarousel />
     );
 };
 
