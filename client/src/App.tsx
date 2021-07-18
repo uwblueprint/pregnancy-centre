11b91d8c1e5a41dc940cd9d61714196e8be41663
Megan Niu <megan.niu@gmail.com>
2021-07-18 00:13:18 -0400

    Remove /test route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,8 +10,6 @@ import SendResetPasswordEmailModal from "./pages/SendResetPasswordEmailModal";
 import SignInModal from "./pages/SignInModal";
 import SignUpModal from "./pages/SignUpModal";
 
-import DonationFormConfirmationPageContainer from "./components/examples/DonationFormConfirmationPageContainer";
-
 function App(): JSX.Element {
     return (
         <span>
@@ -26,9 +24,6 @@ function App(): JSX.Element {
                     <Route path="/admin" component={AdminHomepage}></Route>
                     <Route path="/request-group/:id">
                         <AdminRequestGroupView />
-                    </Route>
-                    <Route path="/test">
-                        <DonationFormConfirmationPageContainer />
                     </Route>
                     <Route path="/">
                         <DonorHomepage />
