cdeeaaf06c3a33981d5aa9008736344ee24b8525
Bonnie Chin <bonniechin.mail@gmail.com>
2021-08-29 19:29:27 -0400

    styling edit testimonials card

--- client/src/components/atoms/EditTestimonialCard.tsx
+++ client/src/components/atoms/EditTestimonialCard.tsx
@@ -1,5 +1,8 @@
 import { Col, Row } from "react-bootstrap";
 import React, { FunctionComponent, useState } from "react";
+import { Button } from "../atoms/Button";
+import DefaultImage from "../../assets/grey-background.png"
+import OverlayImage from "../../assets/image-upload.png"
 import TextArea from "../atoms/TextArea";
 
 interface Props {
@@ -13,16 +16,56 @@ interface Props {
 }
 
 const EditTestimonialCard: FunctionComponent<Props> = (props: Props) => {
+    const [testimonial, setTestimonial] = useState(props.testimonial);
+    const [imagePath, setImagePath] = props.imagePath === "" ? useState(DefaultImage) : useState(props.imagePath);
+    const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setTestimonial(event.target.value);
+    }
+    const textAreaProps = {
+        isErroneous: false,
+        onChange: onChange,
+        value: testimonial,
+        placeholder: "",
+        label: "Edit Client Story",
+        maxNumChars: 400
+    }
+
+    const primaryBtnProps = {
+        className: "update-button",
+        text: "Update",
+        onClick: () => props.onSave(props.id, imagePath, testimonial),
+        copyText: ""
+    }
+
+    const secondaryBtnProps = {
+        className: "cancel-button",
+        text: "Cancel",
+        onClick: () => props.onCancel(),
+        copyText: ""
+    }
+
     return (
         <div className="edit-testimonial-card">
             {/* image logic here */}
-            <Row>
-                <Col></Col>
-                <Col></Col>
+            <Row className="content">
+                <Col>
+                    <div className="image-upload">
+                        <img className="base" src={imagePath}/>
+                        <div className="overlay">
+                            <img className="overlay-image" src={OverlayImage}/>
+                            <p className="overlay-text">Add Photo</p>
+                        </div>
+                    </div>
+                </Col>
+                <Col className="text-and-submission">
+                    <TextArea {...textAreaProps} />
+                    <div className="buttons" >
+                        <Button {...primaryBtnProps} />
+                        <Button {...secondaryBtnProps} />
+                        <i className="bi bi-trash" onClick={() => props.onDelete(props.id)} />
+                    </div>
+                </Col>
             </Row>
-            <div className="text-editor">
-                <h3 className="header">Edit Client Story</h3>
-            </div>
         </div>
     );
 };
