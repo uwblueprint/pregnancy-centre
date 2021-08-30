f820a3fee0ddf803ab9504aac9cdcee251c2868b
Bonnie Chin <bonniechin.mail@gmail.com>
2021-08-30 13:49:18 -0400

    schema.ts formatting

--- server/api/schema.ts
+++ server/api/schema.ts
@@ -1,10 +1,12 @@
-import { gql } from "apollo-server";
+import { gql }
+ from "apollo-server";
 
 const typeDefs = gql`
     type DonationFormContributionTuple {
         donationForm: ID
         quantity: Int
     }
+
     type Request {
         _id: ID
         quantity: Int
@@ -18,29 +20,35 @@ const typeDefs = gql`
         deleted: Boolean
         fulfilled: Boolean
     }
+
     input CreateRequestInput {
         quantity: Int
         requestType: ID!
         clientName: String
     }
+
     input UpdateRequestInput {
         _id: ID!
         quantity: Int
         requestType: ID
         clientName: String
     }
+
     input UpdateRequestsInput {
         _id: ID!
         matchedDonations: [DonationFormContributionTupleInput]
     }
+
     input DonationFormContributionTupleInput {
         donationForm: ID
         quantity: Int
     }
+
     # ---  Left as a proof of concept: ---
     # input FilterRequestInput {
     #     NOT_AVAILABLE: Boolean
     # }
+
     type RequestType {
         _id: ID
         name: String
@@ -57,21 +65,25 @@ const typeDefs = gql`
         nextRequest: Request
         nextRecipient: String
     }
+
     input CreateRequestTypeInput {
         name: String!
         requestGroup: ID!
         requests: [ID]
     }
+
     input UpdateRequestTypeInput {
         _id: ID!
         name: String
         requestGroup: ID
         requests: [ID]
     }
+
     # ---  Left as a proof of concept: ---
     # input FilterRequestTypeInput {
     #     NOT_AVAILABLE: Boolean
     # }
+
     type RequestGroup {
         _id: ID
         name: String
@@ -88,11 +100,13 @@ const typeDefs = gql`
         nextRecipient: String
         hasAnyRequests: Boolean
     }
+
     input CreateRequestGroupInput {
         name: String!
         description: String
         image: String
     }
+
     input UpdateRequestGroupInput {
         _id: ID!
         name: String
@@ -100,13 +114,16 @@ const typeDefs = gql`
         image: String
         requestTypes: [ID]
     }
+
     # ---  Left as a proof of concept: ---
     # input FilterRequestGroupInput {
     #     NOT_AVAILABLE: Boolean
     # }
+
     # input FilterOptions {
     #     NOT_AVAILABLE: Boolean
     # }
+
     input DonationFormFilterOptions {
         deleted: Boolean
         requestGroup: ID
@@ -114,14 +131,17 @@ const typeDefs = gql`
         status: DonationItemStatus
         statusNot: DonationItemStatus
     }
+
     enum DonationFormSortOptions {
         CREATED_AT
         MATCHED_AT
     }
+
     enum DonationItemType {
         SPECIFIC
         GENERIC
     }
+
     enum DonationItemCondition {
         BRAND_NEW
         GREAT
@@ -129,24 +149,28 @@ const typeDefs = gql`
         FAIR
         POOR
     }
+
     enum DonationItemStatus {
         PENDING_APPROVAL
         PENDING_DROPOFF
         PENDING_MATCH
         MATCHED
     }
+
     type DonationFormContact {
         firstName: String
         lastName: String
         email: String
         phoneNumber: String
     }
+
     input DonationFormContactInput {
         firstName: String
         lastName: String
         email: String
         phoneNumber: String
     }
+
     type DonationForm {
         _id: ID
         contact: DonationFormContact
@@ -166,6 +190,7 @@ const typeDefs = gql`
         updatedAt: String
         matchedAt: String
     }
+
     input CreateDonationFormInput {
         contact: DonationFormContactInput
         name: String!
@@ -177,6 +202,7 @@ const typeDefs = gql`
         status: DonationItemStatus
         quantityRemaining: Int!
     }
+
     input UpdateDonationFormInput {
         _id: ID!
         name: String
@@ -188,52 +214,62 @@ const typeDefs = gql`
         donatedAt: String
         matchedAt: String
     }
+
     type MapPoint {
         x: Float!
         y: Float!
     }
+
     type Testimonial {
         id: Int
         imagePath: String
         testimonial: String
     }
+
     input TestimonialInput {
         id: Int
         imagePath: String
         testimonial: String
     }
+
     type Map {
         defaultMarkerSize: String!
         markerSizes: [String]!
         points: [MapPoint]!
         testimonials: [Testimonial]
     }
+
     enum StatisticType {
         REGULAR_DONORS
         DIAPERS_DISTRIBUTED
         CARE_CLOSET_VISITS
     }
+
     type Statistic {
         icon: String!
         measurement: String!
         stat: String!
         type: StatisticType!
     }
+
     input StatisticMeasurement {
         REGULAR_DONORS: String
         DIAPERS_DISTRIBUTED: String
         CARE_CLOSET_VISITS: String
     }
+
     type TestimonialCarousel {
         testimonials: [Testimonial]
         interval: Int!
     }
+
     type Banner {
         header: String!
         description: String!
         imagePaths: [String]!
         interval: Int!
     }
+
     type DonorHomepage {
         _id: ID!
         map: Map!
@@ -241,6 +277,7 @@ const typeDefs = gql`
         banner: Banner!
         testimonialCarousel: TestimonialCarousel!
     }
+
     type Query {
         request(_id: ID): Request
         requests: [Request]
@@ -249,19 +286,22 @@ const typeDefs = gql`
         openRequests: [Request]
         # --- Left as a proof of concept: ---
         # requestsFilter(filter: FilterRequestInput, options: FilterOptions): [Request]
+        
         requestType(_id: ID): RequestType
         requestTypes: [RequestType]
         requestTypesPage(skip: Int, limit: Int, open: Boolean): [RequestType]
         countRequestTypes(open: Boolean): Int
         # --- Left as a proof of concept: ---
         # requestTypesFilter(filter: FilterRequestTypeInput, options: FilterOptions): [RequestType]
+        
         requestGroup(_id: ID): RequestGroup
         requestGroups: [RequestGroup]
         requestGroupsPage(skip: Int, limit: Int, name: String, open: Boolean): [RequestGroup]
-        countRequestGroups(open: Boolean, name: String): Int
+        countRequestGroups(open: Boolean, name: String): Int        
         # --- Left as a proof of concept: ---
         # requestGroupsFilter(filter: FilterRequestGroupInput, options: FilterOptions): [RequestGroup]
         requestGroupsFilterByName(filterString: String): [RequestGroup]
+
         donationForm(_id: ID): DonationForm
         donationForms: [DonationForm]
         donationFormsPage(
@@ -270,12 +310,14 @@ const typeDefs = gql`
             filterOptions: DonationFormFilterOptions
             sortBy: DonationFormSortOptions
         ): [DonationForm]
+        
         donorHomepageBanner: Banner
         donorHomepageTestimonialCarousel: TestimonialCarousel
         donorHomepageMap: Map
         donorHomepageStatistics: [Statistic]
         donorHomepage: DonorHomepage
     }
+
     type Mutation {
         createRequest(request: CreateRequestInput): Request
         updateRequest(request: UpdateRequestInput): Request
@@ -283,27 +325,32 @@ const typeDefs = gql`
         deleteRequest(_id: ID): Request
         fulfillRequest(_id: ID): Request
         unfulfillRequest(_id: ID): Request
+        
         changeRequestTypeForRequest(requestId: ID, requestTypeId: ID): Request
         createRequestType(requestType: CreateRequestTypeInput): RequestType
         updateRequestType(requestType: UpdateRequestTypeInput): RequestType
         deleteRequestType(_id: ID): RequestType
+        
         changeRequestGroupForRequestType(requestTypeId: ID, requestGroupId: ID): Request
         createRequestGroup(requestGroup: CreateRequestGroupInput): RequestGroup
         updateRequestGroup(requestGroup: UpdateRequestGroupInput): RequestGroup
         deleteRequestGroup(_id: ID): RequestGroup
+
         createDonationForm(donationForm: CreateDonationFormInput): DonationForm
         updateDonationForm(donationForm: UpdateDonationFormInput): DonationForm
         deleteDonationForm(_id: ID): DonationForm
         changeDonationFormQuantity(_id: ID, quantity: Int): DonationForm
+
         sendConfirmationEmail(ids: [ID]): String
         sendApprovalEmail(id: ID): String
         sendRejectionEmail(id: ID): String
+        
         updateDonorHomepage(
             mapTestimonials: [TestimonialInput!]
             carouselTestimonials: [TestimonialInput!]
             statMeasurements: StatisticMeasurement
         ): DonorHomepage
-    }
-`;
+    }`;
 
-export { typeDefs };
+export { typeDefs }
+;
