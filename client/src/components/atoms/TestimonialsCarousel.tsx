03ea1b37b69c8e66f00864b6b25757ea13a08000
Bonnie Chin <bonniechin.mail@gmail.com>
2021-08-30 22:27:42 -0400

    add quotation marks to testimonials

--- client/src/components/atoms/TestimonialsCarousel.tsx
+++ client/src/components/atoms/TestimonialsCarousel.tsx
@@ -21,7 +21,7 @@ const TestimonialsCarousel: FunctionComponent<Props> = (props: Props) => {
                                 <img src={testimonial.imagePath}></img>
                             </Col>
                             <Col md={12} lg={6} className="my-auto">
-                                <p>{testimonial.testimonial}</p>
+                                <p>{"\"" + testimonial.testimonial + "\""}</p>
                             </Col>
                         </Row>
                     </Carousel.Item>
